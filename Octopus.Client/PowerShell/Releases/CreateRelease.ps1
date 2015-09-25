9913184bfd309449ca0386dbf2d20cc5c890997d
Shane Gill <tothegills@gmail.com>
2015-09-25 14:07:55 +1000

    Create CreateRelease.ps1

---
+++ Octopus.Client/PowerShell/Releases/CreateRelease.ps1
@@ -0,0 +1,29 @@
+# You can this dll from your Octopus Server/Tentacle installation directory or from
+# https://www.nuget.org/packages/Octopus.Client/
+Add-Type -Path 'Octopus.Client.dll' 
+
+$apikey = 'API-MCPLE1AQM2VKTRFDLIBMORQHBXA' # Get this from your profile
+$octopusURI = 'http://localhost' # Your server address
+
+$projectId = "Projects-1" # Get this from /api/projects
+
+$endpoint = new-object Octopus.Client.OctopusServerEndpoint $octopusURI,$apikey 
+$repository = new-object Octopus.Client.OctopusRepository $endpoint
+
+$project = $repository.Projects.Get($projectId)
+$process = $repository.DeploymentProcesses.Get($project.DeploymentProcessId)
+$template = $repository.DeploymentProcesses.GetTemplate($process)
+
+$release = new-object Octopus.Client.Model.ReleaseResource
+$release.Version = $template.NextVersionIncrement
+$release.ProjectId = $project.Id
+
+foreach ($package in $template.Packages)
+{
+    $selectedPackage = new-object Octopus.Client.Model.SelectedPackage
+    $selectedPackage.StepName = $package.StepName
+    $selectedPackage.Version = $package.VersionSelectedLastRelease # Select a new version if you know it
+    $release.SelectedPackages.Add($selectedPackage)
+}
+
+$repository.Releases.Create($release)	
