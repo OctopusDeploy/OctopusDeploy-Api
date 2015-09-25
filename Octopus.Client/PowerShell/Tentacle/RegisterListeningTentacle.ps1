faec81455f701e78c54489ab9c0fb4ad55881fa0
Shane Gill <tothegills@gmail.com>
2015-09-25 13:25:05 +1000

    Add some comments

--- Octopus.Client/PowerShell/Tentacle/RegisterListeningTentacle.ps1
+++ Octopus.Client/PowerShell/Tentacle/RegisterListeningTentacle.ps1
@@ -1,19 +1,21 @@
+# You can this dll from your Octopus Server/Tentacle installation directory or from
+# https://www.nuget.org/packages/Octopus.Client/
 Add-Type -Path 'Octopus.Client.dll' 
 
-$apikey = 'API-xxx' 
-$octopusURI = 'http://localhost'
+$apikey = 'API-xxx' # Get this from your profile
+$octopusURI = 'http://localhost' # Your Octopus Server address
 
 $endpoint = New-Object Octopus.Client.OctopusServerEndpoint $octopusURI,$apikey 
 $repository = New-Object Octopus.Client.OctopusRepository $endpoint
 
 $tentacleEndpoint = New-Object Octopus.Client.Model.EndPoints.ListeningTentacleEndpointResource
-$tentacleEndpoint.Thumbprint = "551290ED75D2A4AEBBB6F31778DB1C0D4865B091"
-$tentacleEndpoint.Uri = "https://localhost:10933"
+$tentacleEndpoint.Thumbprint = "551290ED75D2A4AEBBB6F31778DB1C0D4865B091" # Your Octopus Server thumbprint
+$tentacleEndpoint.Uri = "https://localhost:10933" # Your Tentacle address
 
 $tentacle = New-Object Octopus.Client.Model.MachineResource
 $tentacle.Endpoint = $tentacleEndpoint
-$tentacle.EnvironmentIds.Add("Environments-1")
-$tentacle.Roles.Add("demo-role")
-$tentacle.Name = "Tentacle from client"
+$tentacle.EnvironmentIds.Add("Environments-1") # Get this from /api/environments
+$tentacle.Roles.Add("demo-role") # The role for this machine
+$tentacle.Name = "Demo tentacle" # The name of this machine
 
 $repository.Machines.Create($tentacle)
