ac87be4e5366b5d1f6e2199dd72d44f790d31e98
Shane Gill <tothegills@gmail.com>
2015-09-25 13:54:34 +1000

    Update ScopeRoleToStep.ps1

--- Octopus.Client/PowerShell/DeploymentProcesses/ScopeRoleToStep.ps1
+++ Octopus.Client/PowerShell/DeploymentProcesses/ScopeRoleToStep.ps1
@@ -5,15 +5,18 @@ Add-Type -Path 'Octopus.Client.dll'
 $apikey = 'API-MCPLE1AQM2VKTRFDLIBMORQHBXA' # Get this from your profile
 $octopusURI = 'http://localhost' # Your server address
 
+$projectName = "Demo project" # The name of your project
+$roleName = "demo-role" # The role this step will be scoped to
+
 $endpoint = new-object Octopus.Client.OctopusServerEndpoint $octopusURI,$apikey 
 $repository = new-object Octopus.Client.OctopusRepository $endpoint
 
-$project = $repository.Projects.FindByName("Demo Project") #The name of your project
+$project = $repository.Projects.FindByName($projectName) 
 
 $deploymentProcess = $repository.DeploymentProcesses.Get($project.DeploymentProcessId)
 
 foreach ($step in $deploymentProcess.Steps)
 {
-  $step.Properties.'Octopus.Action.TargetRoles' = "demo-role" # The role this step will be scoped to
+  $step.Properties.'Octopus.Action.TargetRoles' = $roleName
 }
 $repository.DeploymentProcesses.Modify($deploymentProcess)
