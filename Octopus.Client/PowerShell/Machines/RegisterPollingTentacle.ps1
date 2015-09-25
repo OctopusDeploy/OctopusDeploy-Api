9476b477dbf7976346bf2059f0e2a8f12dd9a228
Shane Gill <tothegills@gmail.com>
2015-09-25 13:58:35 +1000

    Update RegisterPollingTentacle.ps1

--- Octopus.Client/PowerShell/Machines/RegisterPollingTentacle.ps1
+++ Octopus.Client/PowerShell/Machines/RegisterPollingTentacle.ps1
@@ -5,17 +5,22 @@ Add-Type -Path 'Octopus.Client.dll'
 $apikey = 'API-xxx' # Get this from your profile
 $octopusURI = 'http://localhost' # Your Octopus Server address
 
+$octopusServerThumbprint = "3BE9C24663D3CE052CFF9D0591914FADB8DEAF30" # Your Octopus Server thumbprint
+$environmentId = "Environments-1" # Get this from /api/environments
+$role = "demo-role" # The role of the machine
+$machineName = "Demo tentacle" # The name of the machine
+
 $endpoint = New-Object Octopus.Client.OctopusServerEndpoint $octopusURI,$apikey 
 $repository = New-Object Octopus.Client.OctopusRepository $endpoint
 
 $tentacleEndpoint = New-Object Octopus.Client.Model.Endpoints.PollingTentacleEndpointResource
-$tentacleEndpoint.Thumbprint = "3BE9C24663D3CE052CFF9D0591914FADB8DEAF30" # Your Octopus Server thumbprint
+$tentacleEndpoint.Thumbprint = $octopusServerThumbprint
 $tentacleEndpoint.Uri = "poll://" + (([char[]]([char]'A'..[char]'Z') | sort {get-random})[0..20] -Join '') + "/"
 
 $tentacle = New-Object Octopus.Client.Model.MachineResource
 $tentacle.Endpoint = $tentacleEndpoint
-$tentacle.EnvironmentIds.Add("Environments-1") # Get this from /api/environments
-$tentacle.Roles.Add("demo-role") # The role of the machine
-$tentacle.Name = "Demo tentacle" # The name of the machine
+$tentacle.EnvironmentIds.Add($environmentId) 
+$tentacle.Roles.Add($role) 
+$tentacle.Name = $machineName
 
 $repository.Machines.Create($tentacle)
