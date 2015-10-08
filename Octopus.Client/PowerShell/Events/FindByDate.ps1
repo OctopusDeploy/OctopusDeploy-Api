b1977798c705f66e87b52495bd1acb7b010a4286
Dalmiro <dalmiro.granias@gmail.com>
2015-10-08 19:01:59 -0300

    adding events/findbydate

---
+++ Octopus.Client/PowerShell/Events/FindByDate.ps1
@@ -0,0 +1,19 @@
+﻿# You can this dll from your Octopus Server/Tentacle installation directory or from
+# https://www.nuget.org/packages/Octopus.Client/
+Add-Type -Path 'Octopus.Client.dll' 
+
+$apikey = '' #Your API Key
+$octopusURI = '' # Your server address
+
+$endpoint = new-object Octopus.Client.OctopusServerEndpoint $octopusURI,$apikey 
+$repository = new-object Octopus.Client.OctopusRepository $endpoint
+
+[System.DateTimeOffset]$after = "10/7/2015"
+[System.DateTimeOffset]$before = "10/8/2015"
+
+#Using lambda expression to filter events using the FindMany method
+$repository.Events.FindMany(
+    {param($e) if(($e.Occurred -gt $after) -and ($e.Occurred -lt $before)){
+        $true
+        }
+    })
