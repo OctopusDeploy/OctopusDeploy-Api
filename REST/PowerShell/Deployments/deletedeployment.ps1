2be54a3ce6ac73a24ebeb727fa5d0113a803240d
Dalmiro <dalmiro.granias@gmail.com>
2015-10-21 14:57:29 -0300

    adding help text

--- REST/PowerShell/Deployments/deletedeployment.ps1
+++ REST/PowerShell/Deployments/deletedeployment.ps1
@@ -2,6 +2,8 @@
 $octopusURL = ""
 $header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
 
+#To get the deployment ID, click on the deployment and you'll reach a URL similar to this: http://localhost/app#/projects/Projects-2/releases/1.0.142/deployments/Deployments-744
+#The deployment ID in that case is Deployments-744
 $deploymentID = ""
 
-Invoke-RestMethod "$env:octopusURL/api/deployments/$deploymentID" -Method Delete -Headers $header
+Invoke-RestMethod "$octopusURL/api/deployments/$deploymentID" -Method Delete -Headers $header
