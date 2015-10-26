7d46ff58d5011ea180f3566fff97762aef33f1f0
Dalmiro <dalmiro.granias@gmail.com>
2015-10-26 14:22:52 -0300

    adding CheckHealthOfSpecificTentacle

---
+++ REST/PowerShell/Targets/CheckHealthOfSpecificTargets.ps1
@@ -0,0 +1,18 @@
+﻿$OctopusURL = ""
+
+$OctopusAPIKey = ""
+
+$header = @{ "X-Octopus-ApiKey" = $OctopusAPIKey }
+
+$machineId = "" #ID of the machine you want to check health on
+
+$body = @{
+    Name = "Health"
+    Description = "Checking health of $machineId"
+    Arguments = @{
+        Timeout= "00:05:00"
+        MachineIds = @($machineId)        
+    }
+} | ConvertTo-Json
+
+Invoke-RestMethod $OctopusURL/api/tasks -Method Post -Body $body -Headers $header
