7f438e72b2a1f010b33dda18943e480952c30809
Dalmiro <dalmiro.granias@gmail.com>
2015-10-27 16:47:07 -0300

    Script now allows to find machine by name
    Kudos to Lance for this improvement
    
    http://help.octopusdeploy.com/discussions/questions/6098

--- REST/PowerShell/Targets/CheckHealthOfSpecificTargets.ps1
+++ REST/PowerShell/Targets/CheckHealthOfSpecificTargets.ps1
@@ -1,18 +1,24 @@
-﻿$OctopusURL = ""
+﻿Add-Type -Path "" # Path to Newtonsoft.Json.dll 
+Add-Type -Path "" # Path to Octopus.Client.dll 
 
-$OctopusAPIKey = ""
+$OctopusURL = "" 
+$OctopusAPIKey = "" 
+$MachineName = "" #Machine Display Name 
+
+$endpoint = new-object Octopus.Client.OctopusServerEndpoint $OctopusURL,$OctopusAPIKey 
+$repository = new-object Octopus.Client.OctopusRepository $endpoint 
+$findmachine = $repository.Machines.FindByName("$MachineName") 
+$machineid = $findmachine.id
 
 $header = @{ "X-Octopus-ApiKey" = $OctopusAPIKey }
 
-$machineId = "" #ID of the machine you want to check health on
-
-$body = @{
-    Name = "Health"
-    Description = "Checking health of $machineId"
-    Arguments = @{
-        Timeout= "00:05:00"
-        MachineIds = @($machineId)        
-    }
+$body = @{ 
+    Name = "Health" 
+    Description = "Checking health of $MachineName" 
+    Arguments = @{ 
+        Timeout= "00:05:00" 
+        MachineIds = @($machineId) #$MachinID could contain an array of machines too
+    } 
 } | ConvertTo-Json
 
 Invoke-RestMethod $OctopusURL/api/tasks -Method Post -Body $body -Headers $header
