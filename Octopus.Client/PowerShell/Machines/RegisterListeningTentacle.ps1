6adda2bbea14ebf30b5545446ad88db6ed3da44b
Shane Gill <tothegills@gmail.com>
2015-10-28 13:01:47 +1000

    Update RegisterListeningTentacle.ps1

--- Octopus.Client/PowerShell/Machines/RegisterListeningTentacle.ps1
+++ Octopus.Client/PowerShell/Machines/RegisterListeningTentacle.ps1
@@ -5,7 +5,7 @@ Add-Type -Path 'Octopus.Client.dll'
 $apikey = 'API-xxx' # Get this from your profile
 $octopusURI = 'http://localhost' # Your Octopus Server address
 
-$octopusServerThumbprint = "551290ED75D2A4AEBBB6F31778DB1C0D4865B091" # Your Octopus Server thumbprint
+$tentacleThumbprint = "551290ED75D2A4AEBBB6F31778DB1C0D4865B091" # Your Tentacle thumbprint
 $tentacleUri = "https://localhost:10933" # Your Tentacle address
 $environmentId = "Environments-1" # Get this from /api/environments
 $role = "demo-role" # The role for this machine
@@ -15,7 +15,7 @@ $endpoint = New-Object Octopus.Client.OctopusServerEndpoint $octopusURI,$apikey
 $repository = New-Object Octopus.Client.OctopusRepository $endpoint
 
 $tentacleEndpoint = New-Object Octopus.Client.Model.EndPoints.ListeningTentacleEndpointResource
-$tentacleEndpoint.Thumbprint = $octopusServerThumbprint
+$tentacleEndpoint.Thumbprint = $tentacleThumbprint
 $tentacleEndpoint.Uri = $tentacleUri
 
 $tentacle = New-Object Octopus.Client.Model.MachineResource
