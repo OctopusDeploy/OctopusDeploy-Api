3127e2c9b43562a7bff1610cc2cc9f1d9d95acc2
Dalmiro <dalmiro.granias@gmail.com>
2015-11-24 19:18:12 -0300

    Update ApproveOrAbort.ps1

--- REST/PowerShell/ManualInterventions/ApproveOrAbort.ps1
+++ REST/PowerShell/ManualInterventions/ApproveOrAbort.ps1
@@ -1,12 +1,12 @@
 ﻿$OctopusURL = "" #Octopus server URL
 $OctopusAPIKey = "" #Octopus API Key
-$InterruptionID = "" #You can get this ID from the deployment document like this -> /api/interruptions?regarding=[Deployment ID]
+$InterruptionID = "" # i.e "Interruptions-204". You can get this ID from the deployment document like this -> /api/interruptions?regarding=[Deployment ID]
 
 $header = @{ "X-Octopus-ApiKey" = $OctopusAPIKey }
 
 $body = @{Instructions= ""
             Notes = "Message" #message 
-            Result="Proceed" #Set this property to "Abort" to abourt the deployment           
+            Result="Proceed" #Set this property to "Abort" to abort the deployment           
         } | ConvertTo-Json
 
 #Take responsability for the Intervention
