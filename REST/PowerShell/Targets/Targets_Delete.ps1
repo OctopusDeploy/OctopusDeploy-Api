1d158183c4cdc7353b7539bb36db76bfd426ccc8
Dalmiro <dalmiro.granias@gmail.com>
2016-01-22 12:57:54 -0300

    Adding Targets_Delete

---
+++ REST/PowerShell/Targets/Targets_Delete.ps1
@@ -0,0 +1,16 @@
+﻿# Data
+$OctopusURL = "" #Octopus Root URL
+$OctopusAPIKey = "" #Your Octopus API Key
+$MachineID = "" #The ID of the machine you want to delete. e.g "Machines-1"
+
+# Delete function
+Function Delete-OctopusTarget([string]$ID) {
+   return Invoke-RestMethod -Uri "$OctopusUrl/api/machines/$ID`?apiKey=$OctopusApiKey" -Method Delete
+}
+
+# Invoke delete function
+Delete-OctopusTarget -ID $MachineID
+
+
+
+
