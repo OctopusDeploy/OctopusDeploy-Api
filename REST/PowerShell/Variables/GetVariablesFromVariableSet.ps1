385513983f4c8fcd3922013f43607647bce37572
Dalmiro <dalmiro.granias@gmail.com>
2016-01-25 12:07:47 -0300

    Adding GetVariablesFromVariablesSet

---
+++ REST/PowerShell/Variables/GetVariablesFromVariableSet.ps1
@@ -0,0 +1,15 @@
+﻿##Config
+$OctopusAPIkey = ""#Octopus API Key
+
+$OctopusURL = ""#Octopus URL
+
+$variableSetName = "Whatever" #Name of the variable set
+
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+
+##Process
+$VariableSet = (Invoke-WebRequest "$OctopusURL/api/libraryvariablesets?contentType=Variables" -Headers $header).content | ConvertFrom-Json | select -ExpandProperty Items | ?{$_.name -eq $variableSetName}
+
+$variables = (Invoke-WebRequest "$OctopusURL/$($VariableSet.Links.Variables)" -Headers $header).content | ConvertFrom-Json | select -ExpandProperty Variables
+
+$variables #<--- Collection of variables of the variable set
