7be516b5c569bbe2019196177ef6352dc2c29ad3
Dalmiro <dalmiro.granias@gmail.com>
2016-02-24 12:26:11 -0300

    Adding CancelDeployment.ps1

---
+++ REST/PowerShell/Deployments/CancelDeployment.ps1
@@ -0,0 +1,26 @@
+﻿##CONFIG##
+
+$OctopusURL = "" #Octopus URL
+$OctopusAPIKey = "" #Octopus API Key
+
+$ProjectName = "" #Name of the project that owns the deployment
+$EnvironmentName = "" #Name of the environment where the deployment is taking place
+
+
+##PROCESS##
+
+$header = @{ "X-Octopus-ApiKey" = $OctopusAPIKey }
+
+#Get dashboard to get latest deployments
+$dashboard = (Invoke-WebRequest "$OctopusURL/api/dashboard" -Method Get -Headers $header).content | ConvertFrom-Json
+
+#Get the Environment and Project to filter later
+$Environment = $dashboard.Environments | ?{$_.name -eq $EnvironmentName}
+$Project = $dashboard.Projects | ?{$_.name -eq $ProjectName}
+
+#Get the deployment
+$Deployment = $dashboard.Items | ?{($_.ProjectID -eq $Project.Id) -and ($_.EnvironmentID -eq $Environment.id)} | sort -Descending -Property Created |select -First 1
+
+
+#Cancel the task asociated with the deployment
+Invoke-WebRequest ("$OctopusURL" + $Deployment.links.Task + "/cancel") -Method Post -Headers $header
