905b767ce263b3ec81b5b046836e39100f5c9b90
Michael Noonan <michaelnoonan@gmail.com>
2016-04-06 09:38:15 +1000

    Use PollingTentacleEndpointResource!

--- Octopus.Client/Csharp/Machines/RegisterPollingTentacle.cs
+++ Octopus.Client/Csharp/Machines/RegisterPollingTentacle.cs
@@ -3,7 +3,7 @@ static void Main(string[] args)
   var endpoint = new OctopusServerEndpoint("http://localhost/", "API-XXXXXXXXXXXXXXXXXXXXXXXXXX");
   var repository = new OctopusRepository(endpoint);
 
-  var tentacleEndpoint = new ListeningTentacleEndpointResource();
+  var tentacleEndpoint = new PollingTentacleEndpointResource();
   tentacleEndpoint.Thumbprint = "551290ED75D2A4AEBBB6F31778DB1C0D4865B091"; // The certificate thumbprint of the tentacle
   tentacleEndpoint.Uri = "poll://PDMFX3BFMQ1EEB0BOU2Y/"; // A 20-character random alpha-numeric string becomes the mailbox to poll from
 
