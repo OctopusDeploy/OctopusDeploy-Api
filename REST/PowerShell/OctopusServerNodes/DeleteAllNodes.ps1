e5d580dda90aaae06309e4870b377e635cdba980
Shane Gill <tothegills@gmail.com>
2016-07-29 20:45:45 +1000

    Create DeleteAllNodes.ps1

---
+++ REST/PowerShell/OctopusServerNodes/DeleteAllNodes.ps1
@@ -0,0 +1,10 @@
+$octopusURI = "http://localhost:8065" # Replace this with your octopus URL
+$apiKey = "API-1232" # Replace this with your API key http://docs.octopusdeploy.com/display/OD/How+to+create+an+API+key
+
+$header =  @{ "X-Octopus-ApiKey" = $apiKey }
+
+$nodes = Invoke-WebRequest -Uri "$octopusURI/api/octopusservernodes" -Headers $header | ConvertFrom-Json
+$nodes.Items | % {
+    $id = $_.Id
+    Invoke-WebRequest -Uri "$octopusURI/api/octopusservernodes/$id" -Headers $header -Method Delete
+}
