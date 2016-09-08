8443514cc5508aecb98428bc8d83a6059de26952
Dalmirog <dalmiro.granias@gmail.com>
2016-09-08 15:52:20 -0300

    adding script to delete project by name

---
+++ REST/PowerShell/Projects/DeleteProjectByName.ps1
@@ -0,0 +1,20 @@
+﻿##CONFIG##
+$OctopusAPIkey = "" #Octopus API Key
+$OctopusURL = "" #Octopus URL
+$ProjectName = "" #Project to delete
+
+##PROCESS##
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+
+$allprojects = (Invoke-WebRequest $OctopusURL/api/projects/all -Method Get -Headers $header).content | ConvertFrom-Json
+
+$ProjectToDelete = $allprojects | ?{$_.Name -eq $ProjectName}
+
+If(!([string]::IsNullOrEmpty($ProjectToDelete))){
+    Invoke-WebRequest ($OctopusURL + $ProjectToDelete.links.self) -Method Delete -Headers $header
+}
+else{
+    Write-Output "No project found with the name: $ProjectName"
+}
+
+
