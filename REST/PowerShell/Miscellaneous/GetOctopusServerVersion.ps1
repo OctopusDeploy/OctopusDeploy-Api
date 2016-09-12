91094404781eb37beafb944a2c0a99c014ce8d85
Dalmirog <dalmiro.granias@gmail.com>
2016-09-11 22:38:01 -0300

    Adding script to get Octopus Server version

---
+++ REST/PowerShell/Miscellaneous/GetOctopusServerVersion.ps1
@@ -0,0 +1,9 @@
+﻿##CONFIG##
+$OctopusAPIkey = "" #Your Octopus API Key
+$OctopusURL = "" #Your Octopus Server root URL
+
+##PROCESS##
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+$ServerVersion = ((Invoke-WebRequest $OctopusURL/api -Method GET -Headers $header).content | ConvertFrom-Json).version
+$ServerVersion
+
