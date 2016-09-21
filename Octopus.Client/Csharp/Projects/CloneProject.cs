adb0e08a9f635dd267e1512dbdae2a93b7316a90
Dalmirog <dalmiro.granias@gmail.com>
2016-09-21 14:03:30 -0300

    adding snippet to clone projects

---
+++ Octopus.Client/Csharp/Projects/CloneProject.cs
@@ -0,0 +1,13 @@
+﻿var client = new OctopusClient(new OctopusServerEndpoint(octoUrl, apiKey));
+var repo = new OctopusRepository(client);
+
+var project = repo.Projects.FindByName("MyOriginalProject");
+            
+var newProject = new ProjectResource
+{
+    Name = "MyClonedProject",
+    Description = "Cloned copy",
+    ProjectGroupId = project.ProjectGroupId,
+    LifecycleId = project.LifecycleId
+};
+client.Post("~/api/projects?clone=" + project.Id, newProject);
