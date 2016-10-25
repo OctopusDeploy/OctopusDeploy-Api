3a44f5a86843dc003a3df8b9315da4bf78fa5691
Shane Gill <tothegills@users.noreply.github.com>
2016-10-26 09:05:57 +1000

    Create CreateTenant.ps1

---
+++ Octopus.Client/PowerShell/Tenants/CreateTenant.ps1
@@ -0,0 +1,27 @@
+
+# You can this dll from your Octopus Server/Tentacle installation directory or from
+# https://www.nuget.org/packages/Octopus.Client/
+Add-Type -Path 'Octopus.Client.dll' 
+
+$apikey = 'API-xxx' # Get this from your profile
+$octopusURI = 'http://localhost' # Your Octopus Server address
+
+$endpoint = New-Object Octopus.Client.OctopusServerEndpoint $octopusURI, $apiKey
+$repository = New-Object Octopus.Client.OctopusRepository $endpoint
+
+
+$tagSetEditor = $repository.TagSets.CreateOrModify("Hosting")
+$tagSetEditor.AddOrUpdateTag("On premises", "Hosted on site", [Octopus.Client.Model.TagResource+StandardColor]::DarkGreen)
+$tagSetEditor.AddOrUpdateTag("Cloud", "Hosted in the cloud", [Octopus.Client.Model.TagResource+StandardColor]::LightBlue)
+$tagSetEditor.Save()
+
+$tagSet = $tagSetEditor.Instance
+
+
+$project = $repository.Projects.FindByName("Multi tenant project")
+$environment = $repository.Environments.FindByName("Dev")
+
+$tenantEditor = $repository.Tenants.CreateOrModify("John")
+$tenantEditor.WithTag($tagSet.Tags[0])
+$tenantEditor.ConnectToProjectAndEnvironments($project, $environment)
+$tenantEditor.Save()
