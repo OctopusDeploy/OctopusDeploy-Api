a6b2a9711aeeeec7b45c8a367f4316f6e660b848
Matt Richardson <matt.richardson@octopus.com>
2016-11-03 14:55:00 +1100

    Add script to list machines and their upgrade status

---
+++ REST/PowerShell/Machines/ListMachinesWithUpgradeStatus.ps1
@@ -0,0 +1,11 @@
+﻿##CONFIG##
+$OctopusAPIkey = "" #Octopus API Key
+$OctopusURL = "" #Octopus URL
+
+##PROCESS##
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+
+$allMachines = (Invoke-WebRequest $OctopusURL/api/machines/all -Method Get -Headers $header -UseBasicParsing).content | ConvertFrom-Json
+$healthyMachines = $allMachines | ? {$_.HealthStatus -eq "Healthy"}
+
+$healthyMachines | %{ "Machine $($_.Name): UpgradeSuggested=$($_.Endpoint.TentacleVersionDetails.UpgradeSuggested), UpgradeRequired=$($_.Endpoint.TentacleVersionDetails.UpgradeRequired)"} 
