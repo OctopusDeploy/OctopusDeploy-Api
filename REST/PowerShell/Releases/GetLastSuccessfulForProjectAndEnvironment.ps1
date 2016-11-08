dc5e4bbae69ca1bb629aa24eac1b27a9dde71088
Dalmirog <dalmiro.granias@gmail.com>
2016-11-08 15:28:56 -0300

    adding script to get last successful release deployed to the current environment

---
+++ REST/PowerShell/Releases/GetLastSuccessfulForProjectAndEnvironment.ps1
@@ -0,0 +1,14 @@
+﻿##CONFIG
+$OctopusURL = "" #Octopus URL
+$OctopusAPIKey = "" #Octopus API Key
+
+##PROCESS##
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+$ProjectID = ""#$OctopusParameters['Octopus.Project.ID']
+$EnvironmentID = "" #$OctopusParameters['Octopus.Environment.ID']
+
+$ProjectDashboardReleases = (Invoke-WebRequest $OctopusURL/api/progression/$ProjectID -Method Get -Headers $header).content | ConvertFrom-Json
+
+$LastSuccessfullRelease = $ProjectDashboardReleases.Releases.Deployments.$EnvironmentId | ?{$_.state -eq "Success"} | select -First 1
+
+$LastSuccessfullRelease.ReleaseVersion
