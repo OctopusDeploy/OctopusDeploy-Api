ba49932d1849d0c845b04138823886b880dab391
Dalmirog <dalmiro.granias@gmail.com>
2016-11-15 12:19:30 -0300

    adding GetCariableValueFromDeployment

---
+++ REST/PowerShell/Variables/GetVariableValueFromADeployment.ps1
@@ -0,0 +1,13 @@
+﻿##CONFIG
+$OctopusURL = "" #Octopus URL
+$OctopusAPIKey = "" #Octopus API Key
+
+$DeploymentID = "" #ID of the deployment you want to get the variable from. E.g. Deployments-41
+$VariableName = "" #Variable name
+
+##PROCESS##
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+
+$variable = ((invoke-webrequest $OctopusURL/api/variables/variableset-$DeploymentID -Headers $header).content | ConvertFrom-Json).variables | ?{$_.name -eq $VariableName}
+
+$variable.value
