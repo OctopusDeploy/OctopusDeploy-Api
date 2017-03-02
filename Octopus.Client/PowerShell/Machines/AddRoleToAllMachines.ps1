7709407ea988af20f7ce4d4e55e61089fca70fad
Shane Gill <tothegills@users.noreply.github.com>
2017-03-02 11:13:31 +1000

    Create AddRoleToAllMachines.ps1

---
+++ Octopus.Client/PowerShell/Machines/AddRoleToAllMachines.ps1
@@ -0,0 +1,18 @@
+# You can this dll from your Octopus Server/Tentacle installation directory or from
+# https://www.nuget.org/packages/Octopus.Client/
+Add-Type -Path 'Octopus.Client.dll' 
+
+$apikey = 'API-XXXXXXXXXXXXXXXXXXXXXXXXXX' # Get this from your profile
+$octopusURI = 'https://octopus.url' # Your server address
+
+$role = "A new role" # The role to add to each machine
+
+$endpoint = New-Object Octopus.Client.OctopusServerEndpoint $octopusURI,$apikey 
+$repository = New-Object Octopus.Client.OctopusRepository $endpoint
+
+$machines = $repository.Machines.FindAll();
+
+foreach ($machine in $machines) {
+    $machine.Roles.Add($role)
+    $repository.Machines.Modify($machine)
+}
