e07a33e38b1c52980d0554d21ab553c421b12b2d
dalmiro grañas <dalmiro graas>
2017-03-27 17:11:42 -0300

    Adding script to enable or disable machines

---
+++ REST/PowerShell/Targets/EnableOrDisableAMachine.ps1
@@ -0,0 +1,23 @@
+﻿###CONFIG###
+$OctopusURL = "***REMOVED***" #Octopus Server root URL
+$APIKey = "***REMOVED***" #Octopus API Key
+
+$machineName = "South" #Name of the machine to enable/disable
+
+###PROCESS###
+$header = @{ "X-Octopus-ApiKey" = $APIKey }
+
+#Getting all machines
+$allmachines = (Invoke-WebRequest $OctopusURL/api/machines/all -Headers $header).content | ConvertFrom-Json
+
+#Filtering machine by name
+$machine = $allmachines | ?{$_.name -eq $machineName}
+
+#Setting the "IsDisabled" property
+$machine.IsDisabled = $true #Set to $false to disable the machine
+
+#Converting $machine into a JSON blob to PUT is back to the server
+$body = $machine | ConvertTo-Json -Depth 4
+
+#Pushing the modified machine to the userver
+Invoke-WebRequest ($OctopusURL + $machine.Links.Self) -Method Put -Body $body -Headers $header
