d31bec9a00adb2d3fe42ab4615ee90ed7f3d2bf8
Pawel Pabich <pawel@pabich.eu>
2017-05-16 13:16:11 +1000

    Added a script that fixes empty scopes in the variables

---
+++ Octopus.Client/PowerShell/Variables/RemoveEmptyCollectionsFromScope.ps1
@@ -0,0 +1,39 @@
+﻿$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest;
+
+# Data fix for: http://help.octopusdeploy.com/discussions/problems/51848-variable-with-no-scope
+
+# You can this dll from your Octopus Server/Tentacle installation directory or from
+# https://www.nuget.org/packages/Octopus.Client/
+Add-Type -Path "C:\Program Files\Octopus Deploy\Tentacle\Octopus.Client.dll"
+
+$apikey = 'API-HOHYUQSVWOWR3X006TOOJUYU9VW' # You can get this from your profile
+$octopusURI = 'http://localhost:8065' # Your server address
+$projectName = "Variables" # Name of the project where you want to update the variable
+ 
+$endpoint = new-object Octopus.Client.OctopusServerEndpoint ($octopusURI, $apikey)
+$repository = new-object Octopus.Client.OctopusRepository $endpoint
+
+#Get Project
+$project = $repository.Projects.FindByName($projectName)
+
+#Get Project's variable set
+$variableset = $repository.VariableSets.Get($project.links.variables)
+
+#Get variable to update    
+$variables = $variableset.Variables | Where-Object{$_.Scope -ne $null}
+
+foreach($variable in $variables){
+    $keys = @() + $variable.Scope.Keys
+    foreach($propertyName in $keys){
+        $propertyValue = $variable.Scope[$propertyName];
+        if ($propertyValue.Count -eq 0) {
+            Write-Host "Removing empty '$propertyName' scope collection from '$($variable.Name)' variable"
+            $variable.Scope.Remove($propertyName);
+        }
+    }
+}
+
+
+#Save variable set
+$repository.VariableSets.Modify($variableset)
