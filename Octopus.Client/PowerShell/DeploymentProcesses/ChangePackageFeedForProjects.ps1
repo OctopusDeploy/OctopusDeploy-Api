d55e2992034f96b3f7e62d2cbe8c03eb0e9c796e
Shane Gill <tothegills@users.noreply.github.com>
2018-01-29 10:58:13 +1000

    Script to change package feed

---
+++ Octopus.Client/PowerShell/DeploymentProcesses/ChangePackageFeedForProjects.ps1
@@ -0,0 +1,24 @@
+# You can this dll from your Octopus Server/Tentacle installation directory or from
+# https://www.nuget.org/packages/Octopus.Client/
+Add-Type -Path 'Octopus.Client.dll' 
+
+$apikey = 'API-RASSW3YNB6FJL3FNWBSSUUCX0C' # Get this from your profile
+$octopusURI = 'http://localhost:8065' # Your server address
+$projectSearchString = "FOO" # Common string contained in projects to change
+
+$endpoint = New-Object Octopus.Client.OctopusServerEndpoint $octopusURI,$apikey 
+$repository = New-Object Octopus.Client.OctopusRepository $endpoint
+
+$repository.Projects.FindMany({Param($p) $p.Name.Contains($projectSearchString)}) | % {
+    $process = $repository.DeploymentProcesses.Get($_.DeploymentProcessId)
+    $process.Steps | % {
+        $_.Actions | % {
+            if ($_.ActionType -eq "Octopus.TentaclePackage")
+            {
+                Write-Output "Setting feed for step $($_.Name) to the built-in package repository"
+                $_.Properties["Octopus.Action.Package.FeedId"] = "feeds-builtin"
+            }
+        }
+    }
+    $repository.DeploymentProcesses.Modify($process)
+}
