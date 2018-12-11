576854f89d1115dc51038df52dc7a34ceba2bf89
Justin Newman <jnewman@cmnhospitals.org>
2018-12-11 09:16:52 -0700

    Added a script to remove a step from all projects where it exists
    Removes a process step from every project where it exists, as long as it has the same name.

---
+++ Octopus.Client/PowerShell/DeploymentProcesses/RemoveDeploymentProcessStep.ps1
@@ -0,0 +1,25 @@
+﻿# You can this dll from your Octopus Server/Tentacle installation directory or from
+# https://www.nuget.org/packages/Octopus.Client/
+Add-Type -Path 'Octopus.Client.dll' 
+
+$apikey = 'API-MCPLE1AQM2VKTRFDLIBMORQHBXA' # Get this from your profile
+$octopusURI = 'http://localhost' # Your server address
+$stepName = 'Raygun - Register Deployment' # The name of the step you want to remove
+
+$endpoint = New-Object Octopus.Client.OctopusServerEndpoint $octopusURI,$apikey 
+$repository = New-Object Octopus.Client.OctopusRepository $endpoint
+
+$projects = $repository.Projects.FindAll()
+
+foreach ($project in $projects)  {
+    $process = $repository.DeploymentProcesses.Get($project.DeploymentProcessId)
+    $IndexOfStep = $process.Steps.Name.IndexOf($stepName)
+    if($IndexOfStep -ne -1) {
+        $process.Steps.RemoveAt($IndexOfStep)
+
+    }
+    else {
+        Write-Host "'$stepName' does not exist in this project"
+    }
+    $repository.DeploymentProcesses.Modify($process)
+} 
