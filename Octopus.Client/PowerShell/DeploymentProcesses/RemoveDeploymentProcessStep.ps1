30c45ed391816c2e3edb3ff863827229d119aafc
Justin Newman <jnewman@cmnhospitals.org>
2018-12-13 00:28:57 -0700

    Added ability to remove the step from one, many, or all projects

--- Octopus.Client/PowerShell/DeploymentProcesses/RemoveDeploymentProcessStep.ps1
+++ Octopus.Client/PowerShell/DeploymentProcesses/RemoveDeploymentProcessStep.ps1
@@ -4,22 +4,30 @@ Add-Type -Path 'Octopus.Client.dll'
 
 $apikey = 'API-KEY' # Get this from your profile
 $octopusURI = 'https://localhost' # Your server address
+$projectNames = @('') # Name of the Project(s) in which you want to add the step, leave empty string for ALL
 $stepName = '' # The name of the step you want to remove
 
 $endpoint = New-Object Octopus.Client.OctopusServerEndpoint $octopusURI,$apikey 
 $repository = New-Object Octopus.Client.OctopusRepository $endpoint
 
-$projects = $repository.Projects.FindAll()
-
-foreach ($project in $projects)  {
-    $process = $repository.DeploymentProcesses.Get($project.DeploymentProcessId)
-    $IndexOfStep = $process.Steps.Name.IndexOf($stepName)
-    if($IndexOfStep -ne -1) {
-        $process.Steps.RemoveAt($IndexOfStep)
-
+foreach ($name in $projectNames) {
+    if (![string]::IsNullOrEmpty($projectNames)) {
+        $projects = $repository.Projects.FindByName($name)
     }
     else {
-        Write-Host "'$stepName' does not exist in this project" -ForegroundColor Green
+        $projects = $repository.Projects.GetAll()
     }
-    $repository.DeploymentProcesses.Modify($process)
-} 
+
+    foreach ($project in $projects)  {
+        $process = $repository.DeploymentProcesses.Get($project.DeploymentProcessId)
+        $IndexOfStep = $process.Steps.Name.IndexOf($stepName)
+        if($IndexOfStep -ne -1) {
+            $process.Steps.RemoveAt($IndexOfStep)
+
+        }
+        else {
+            Write-Host "'$stepName' does not exist in this project" -ForegroundColor Green
+        }
+        $repository.DeploymentProcesses.Modify($process)
+    } 
+}
