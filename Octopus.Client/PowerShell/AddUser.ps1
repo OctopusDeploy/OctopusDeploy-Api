2710b9e58999a697d242cc95b22fd934ab106079
jackwagons <50080355+jackwagons@users.noreply.github.com>
2019-08-27 08:23:16 -0500

    Create AddUser.ps1

---
+++ Octopus.Client/PowerShell/AddUser.ps1
@@ -0,0 +1,73 @@
+<# 
+This script conditionaly adds user from an AD group to Octo
+#>
+param([Parameter(Mandatory)] [string] $aDGroupName);
+
+$ErrorActionPreference = "Stop";
+
+function get_ad_group_members ([Parameter(Mandatory = $true)] $aDGroupName) {
+    $serverName = [Utility]::get_domaincontroller_name();
+
+    # Fetch ADUSers for the provided Group
+    Get-ADGroupMember -Identity $aDGroupName -server $serverName -Recursive |
+      Get-ADUser -Property DisplayName, EmailAddress | 
+      Select-Object Name, ObjectClass, DisplayName, UserPrincipalName, EmailAddress;    
+}
+
+function get_octo_users_all ([Parameter(Mandatory = $true)] $repository) {
+    $repository.Users.GetAll();
+}
+
+function process_octo_users (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)] $repository, 
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)] $adUsers, 
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)] $allOctoUsers,
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)] $testOnly) {
+   
+    foreach ($adUser in $adUsers) {
+        # Find Octo User by ADUser Email
+        $user = $allOctoUsers | Where-Object { $_.EmailAddress -eq $adUser.EmailAddress };
+    
+        if ($user) {
+            Write-Host "Found" $user.DisplayName -ForegroundColor Gray -BackgroundColor Black;
+        }
+        else {
+            if (!$testOnly) {
+                add_octo_user $adUser $repository;
+            }                                  
+        }
+    }
+}
+
+function add_octo_user (
+    [Parameter(Mandatory = $true)] $adUser, 
+    [Parameter(Mandatory = $true)] $repository) {
+    
+    if ($adUser -and $adUser.EmailAddress) {
+        $userToAdd = New-Object Octopus.Client.Model.UserResource;
+        $userToAdd.Username = $adUser.UserPrincipalName;
+        $userToAdd.DisplayName = $adUser.DisplayName;
+        $userToAdd.EmailAddress = $adUser.EmailAddress;
+        $userToAdd.IsActive = $true;
+        
+        $repository.Users.Create($userToAdd);
+        Write-Host "Added" $userToAdd.DisplayName -ForegroundColor Green -BackgroundColor Black;
+    }
+    else {
+        Write-Warning "Invalid User
+    }
+}
+
+function main {
+    Clear-Host;
+    
+    . '.\PowerShell\Helpers\octo-utility.ps1';
+    $repository = [utility]::new().get_octopus_repository();
+
+    $adUsers = get_ad_group_members $aDGroupName;
+    $allOctoUsers = get_octo_users_all $repository;
+
+    process_octo_users $repository $adUsers $allOctoUsers $testOnly;
+}
+
+main;
