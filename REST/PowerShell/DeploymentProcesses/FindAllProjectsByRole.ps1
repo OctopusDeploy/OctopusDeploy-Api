8bc1efa9ea344acbd9863d88823e9e5e969a2e59
Bob Walker <bob.walker@octopus.com>
2019-10-18 14:34:47 -0500

    Script to find all projects which use that role

---
+++ REST/PowerShell/DeploymentProcesses/FindAllProjectsByRole.ps1
@@ -0,0 +1,28 @@
+﻿$OctopusUrl = "" # Your URL
+$ApiKey = "" # Your API Key
+$roleName = "FileBackup"
+$header = @{ "X-Octopus-ApiKey" = $ApiKey }
+
+$projectList = Invoke-RestMethod "$octopusUrl/api/$spaceId/projects?skip=0&take=10000" -Headers $header
+$projectsWithRoles = @()
+
+foreach ($project in $projectList.Items)
+{
+    $deploymentProcessUrl = $OctopusUrl + $project.Links.DeploymentProcess
+    $projectDeploymentProcess = Invoke-RestMethod $deploymentProcessUrl -Headers $header
+    
+    foreach ($step in $projectDeploymentProcess.Steps)
+    {        
+        if ($step.Properties.'Octopus.Action.TargetRoles' -contains $roleName)
+        {
+            $projectsWithRoles += $project.Name
+            break
+        }
+    } 
+}
+
+Write-Host "The following projects have $roleName"
+foreach ($projectName in $projectsWithRoles)
+{
+    Write-Host $projectName
+}
