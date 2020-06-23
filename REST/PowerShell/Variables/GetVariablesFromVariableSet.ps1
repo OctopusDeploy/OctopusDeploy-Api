94f2087efd308ed67ac94f7ef5c6def22a4e0f60
Alex Pearce <6473983+alexpearce92@users.noreply.github.com>
2020-06-23 15:11:45 -0500

    Update GetVariablesFromVariableSet.ps1
    Adjust request to handle virtual directories

--- REST/PowerShell/Variables/GetVariablesFromVariableSet.ps1
+++ REST/PowerShell/Variables/GetVariablesFromVariableSet.ps1
@@ -1,15 +1,16 @@
 ﻿##Config
-$OctopusAPIkey = ""#Octopus API Key
+$OctopusAPIkey = "" #Octopus API Key
 
-$OctopusURL = ""#Octopus URL
+$OctopusRootURL = "" #Octopus URL. Ex. https://mywebsite.com
+$OctopusVirtualDirs = "" #Any virual directory that follows the octopus root url. Ex. /OctopusDeploy
 
 $variableSetName = "Whatever" #Name of the variable set
 
 $header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
 
 ##Process
-$VariableSet = (Invoke-WebRequest "$OctopusURL/api/libraryvariablesets?contentType=Variables" -Headers $header).content | ConvertFrom-Json | select -ExpandProperty Items | ?{$_.name -eq $variableSetName}
+$VariableSet = (Invoke-WebRequest "$OctopusRootURL$OctopusVirtualDirs/api/libraryvariablesets?contentType=Variables" -Headers $header).content | ConvertFrom-Json | select -ExpandProperty Items | ?{$_.name -eq $variableSetName}
 
-$variables = (Invoke-WebRequest "$OctopusURL/$($VariableSet.Links.Variables)" -Headers $header).content | ConvertFrom-Json | select -ExpandProperty Variables
+$variables = (Invoke-WebRequest "$OctopusRootURL$($VariableSet.Links.Variables)" -Headers $header).content | ConvertFrom-Json | select -ExpandProperty Variables
 
 $variables #<--- Collection of variables of the variable set
