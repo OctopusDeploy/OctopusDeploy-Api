7364fd435d4bd77d209082f676d0858c09ec693f
Mark Harrison <mark.harrison@octopus.com>
2020-09-07 13:40:14 +0100

    CreateReleaseAndDeployment.ps1

---
+++ Octopus.Client/PowerShell/Deployments/CreateReleaseAndDeployment.ps1
@@ -0,0 +1,81 @@
+# You can this dll from your Octopus Server/Tentacle installation directory or from
+# https://www.nuget.org/packages/Octopus.Client/
+Add-Type -Path 'path\to\Octopus.Client.dll'
+
+$octopusBaseURL = "https://youroctourl/"
+$octopusAPIKey = "API-YOURAPIKEY"
+
+$endpoint = New-Object Octopus.Client.OctopusServerEndpoint($octopusBaseURL, $octopusAPIKey)
+$repository = New-Object Octopus.Client.OctopusRepository($endpoint)
+
+$spaceName = "Default"
+$projectName = "Your Project Name"
+$channelName = "Default"
+$environmentName = "Dev"
+
+try {
+    # Get space id
+    $space = $repository.Spaces.FindByName($spaceName)
+    Write-Host "Using Space named $($space.Name) with id $($space.Id)"
+
+    # Create space specific repository
+    $repositoryForSpace = [Octopus.Client.OctopusRepositoryExtensions]::ForSpace($repository, $space)
+
+    # Get project by name
+    $project = $repositoryForSpace.Projects.FindByName($projectName)
+    Write-Host "Using Project named $($project.Name) with id $($project.Id)"
+
+    # Get channel by name
+    $channel = $repositoryForSpace.Channels.FindByName($project, $channelName)
+    Write-Host "Using Channel named $($channel.Name) with id $($channel.Id)"
+
+    # Get environment by name
+    $environment = $repositoryForSpace.Environments.FindByName($environmentName)
+    Write-Host "Using Environment named $($environment.Name) with id $($environment.Id)"
+
+    # Get the deployment process template
+    Write-Host "Fetching deployment process template"
+    $process = $repositoryForSpace.DeploymentProcesses.Get($project.DeploymentProcessId)
+    $template = $repositoryForSpace.DeploymentProcesses.GetTemplate($process, $channel)
+
+    Write-Host "Creating release for $projectName"
+    $release = New-Object Octopus.Client.Model.ReleaseResource -Property @{
+        ChannelId = $channel.Id
+        ProjectId = $project.Id
+        Version   = $template.NextVersionIncrement
+    }
+
+    # Set the package version to the latest for each package
+    # If you have channel rules that dictate what versions can be used,
+    #  you'll need to account for that by overriding the package version
+    Write-Host "Getting action package versions"
+    $template.Packages | ForEach-Object {
+        $feed = $repositoryForSpace.Feeds.Get($_.FeedId)
+        $latestPackage = [Linq.Enumerable]::FirstOrDefault($repositoryForSpace.Feeds.GetVersions($feed, @($_.PackageId)))
+
+        $selectedPackage = New-Object Octopus.Client.Model.SelectedPackage -Property @{
+            ActionName = $_.ActionName
+            Version    = $latestPackage.Version
+        }
+
+        Write-Host "Using version $($latestPackage.Version) for action $($_.ActionName) package $($_.PackageId)"
+
+        $release.SelectedPackages.Add($selectedPackage)
+    }
+
+    # Create release
+    $release = $repositoryForSpace.Releases.Create($release, $false) # pass in $true if you want to ignore channel rules
+
+    # Create deployment
+    $deployment = New-Object Octopus.Client.Model.DeploymentResource -Property @{
+        ReleaseId     = $release.Id
+        EnvironmentId = $environment.Id
+    }
+
+    Write-Host "Creating deployment for release $($release.Version) of project $projectName to environment $environmentName"
+    $deployment = $repositoryForSpace.Deployments.Create($deployment)
+}
+catch {
+    Write-Host $_.Exception.Message
+    exit
+}
--- REST/PowerShell/Deployments/CreateReleaseAndDeployment.ps1
+++ REST/PowerShell/Deployments/CreateReleaseAndDeployment.ps1
@@ -1,35 +1,79 @@
-﻿##CONFIG##
+﻿# Define working variables
+$octopusBaseURL = "https://youroctourl/api"
+$octopusAPIKey = "API-YOURAPIKEY"
+$headers = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
 
-$apiKey = "" #Octopus API Key
-$OctopusURL = "" #Octopus URL
- 
-$ProjectName = "" #project name
-$EnvironmentName = "" #environment name
+$spaceName = "Default"
+$projectName = "Your Project Name"
+$environmentName = "Development"
+$channelName = "Default"
 
-##PROCESS##
+try {
+    # Get space id
+    $spaces = Invoke-WebRequest -Uri "$octopusBaseURL/spaces/all" -Headers $headers -ErrorVariable octoError | ConvertFrom-Json
+    $space = $spaces | Where-Object { $_.Name -eq $spaceName }
+    Write-Host "Using Space named $($space.Name) with id $($space.Id)"
 
-$Header =  @{ "X-Octopus-ApiKey" = $apiKey }
- 
-#Getting Environment and Project By Name
-$Project = Invoke-WebRequest -Uri "$OctopusURL/api/projects/$ProjectName" -Headers $Header| ConvertFrom-Json
- 
-$Environment = Invoke-WebRequest -Uri "$OctopusURL/api/Environments/all" -Headers $Header| ConvertFrom-Json
- 
-$Environment = $Environment | ?{$_.name -eq $EnvironmentName}
+    # Create space specific url
+    $octopusSpaceUrl = "$octopusBaseURL/$($space.Id)"
 
-#Getting Deployment Template to get Next version 
-$dt = Invoke-WebRequest -Uri "$OctopusURL/api/deploymentprocesses/deploymentprocess-$($Project.id)/template" -Headers $Header | ConvertFrom-Json 
- 
-#Creating Release
-$ReleaseBody =  @{ Projectid = $Project.Id
-            version = $dt.nextversionincrement } | ConvertTo-Json
- 
-$r = Invoke-WebRequest -Uri $OctopusURL/api/releases -Method Post -Headers $Header -Body $ReleaseBody | ConvertFrom-Json
- 
-#Creating deployment
-$DeploymentBody = @{ 
-            ReleaseID = $r.Id #mandatory
-            EnvironmentID = $Environment.id #mandatory
-          } | ConvertTo-Json
-          
-$d = Invoke-WebRequest -Uri $OctopusURL/api/deployments -Method Post -Headers $Header -Body $DeploymentBody
+    # Get project by name
+    $projects = Invoke-WebRequest -Uri "$octopusSpaceUrl/projects/all" -Headers $headers -ErrorVariable octoError | ConvertFrom-Json
+    $project = $projects | Where-Object { $_.Name -eq $projectName }
+    Write-Host "Using Project named $($project.Name) with id $($project.Id)"
+
+    # Get channel by name
+    $channels = Invoke-WebRequest -Uri "$octopusSpaceUrl/projects/$($project.Id)/channels" -Headers $headers -ErrorVariable octoError | ConvertFrom-Json
+    $channel = $channels | Where-Object { $_.Name -eq $channelName }
+    Write-Host "Using Channel named $($channel.Name) with id $($channel.Id)"
+
+    # Get environment by name
+    $environments = Invoke-WebRequest -Uri "$octopusSpaceUrl/environments/all" -Headers $headers -ErrorVariable octoError | ConvertFrom-Json
+    $environment = $environments | Where-Object { $_.Name -eq $environmentName }
+    Write-Host "Using Environment named $($environment.Name) with id $($environment.Id)"
+
+    # Get the deployment process template
+    Write-Host "Fetching deployment process template"
+    $template = Invoke-WebRequest -Uri "$octopusSpaceUrl/deploymentprocesses/deploymentprocess-$($project.id)/template?channel=$($channel.Id)" -Headers $headers | ConvertFrom-Json
+
+    # Create the release body
+    $releaseBody = @{
+        ChannelId        = $channel.Id
+        ProjectId        = $project.Id
+        Version          = $template.NextVersionIncrement
+        SelectedPackages = @()
+    }
+
+    # Set the package version to the latest for each package
+    # If you have channel rules that dictate what versions can be used, you'll need to account for that
+    Write-Host "Getting step package versions"
+    $template.Packages | ForEach-Object {
+        $uri = "$octopusSpaceUrl/feeds/$($_.FeedId)/packages/versions?packageId=$($_.PackageId)&take=1"
+        $version = Invoke-WebRequest -Uri $uri -Method GET -Headers $headers -Body $releaseBody -ErrorVariable octoError | ConvertFrom-Json
+        $version = $version.Items[0].Version
+
+        $releaseBody.SelectedPackages += @{
+            ActionName           = $_.ActionName
+            PackageReferenceName = $_.PackageReferenceName
+            Version              = $version
+        }
+    }
+
+    # Create release
+    $releaseBody = $releaseBody | ConvertTo-Json
+    Write-Host "Creating release with these values: $releaseBody"
+    $release = Invoke-WebRequest -Uri $octopusSpaceUrl/releases -Method POST -Headers $headers -Body $releaseBody -ErrorVariable octoError | ConvertFrom-Json
+
+    # Create deployment
+    $deploymentBody = @{
+        ReleaseId     = $release.Id
+        EnvironmentId = $environment.Id
+    } | ConvertTo-Json
+
+    Write-Host "Creating deployment with these values: $deploymentBody"
+    $deployment = Invoke-WebRequest -Uri $octopusSpaceUrl/deployments -Method POST -Headers $headers -Body $deploymentBody -ErrorVariable octoError
+}
+catch {
+    Write-Host "There was an error during the request: $($octoError.Message)"
+    exit
+}
