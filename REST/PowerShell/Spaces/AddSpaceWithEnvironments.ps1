c82467d2bbe56189dc892129095acfdb2e495eb7
Mark Harrison <mark.harrison@octopus.com>
2020-09-07 13:26:43 +0100

    AddSpaceWithEnvironments.ps1

---
+++ REST/PowerShell/Spaces/AddSpaceWithEnvironments.ps1
@@ -0,0 +1,51 @@
+# Define working variables
+$octopusURL = "https://youroctourl"
+$octopusAPIKey = "API-YOURAPIKEY"
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+
+$spaceName = "New Space"
+$description = "Space for the new, top secret project."
+$managersTeams = @() # an array of team Ids to add to Space Managers
+$managerTeamMembers = @() # an array of user Ids to add to Space Managers
+$environments = @('Development', 'Test', 'Production')
+
+$body = @{
+    Name = $spaceName
+    Description = $description
+    SpaceManagersTeams = $managersTeams
+    SpaceManagersTeamMembers = $managerTeamMembers
+    IsDefault = $false
+    TaskQueueStopped = $false
+} | ConvertTo-Json
+
+$response = try {
+    Write-Host "Creating space '$spaceName'"
+    (Invoke-WebRequest $octopusURL/api/spaces -Headers $header -Method Post -Body $body -ErrorVariable octoError)
+} catch [System.Net.WebException] {
+    $_.Exception.Response
+}
+
+if ($octoError) {
+    Write-Host "An error was encountered trying to create the space: $($octoError.Message)"
+    exit
+}
+
+$space = $response.Content | ConvertFrom-Json
+
+foreach ($environment in $environments) {
+    $body = @{
+        Name = $environment
+    } | ConvertTo-Json
+
+    Write-Host "Creating environment '$environment'"
+    $response = try {
+        (Invoke-WebRequest "$octopusURL/api/$($space.Id)/environments" -Headers $header -Method Post -Body $body -ErrorVariable octoError)
+    } catch [System.Net.WebException] {
+        $_.Exception.Response
+    }
+
+    if ($octoError) {
+        Write-Host "An error was encountered trying to create the environment: $($octoError.Message)"
+        exit
+    }
+}
