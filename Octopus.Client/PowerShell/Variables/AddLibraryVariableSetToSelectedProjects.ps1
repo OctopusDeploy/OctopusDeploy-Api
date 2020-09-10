d340e7a6fb0e64b5b117bd666dcfc5f0494dac63
Mark Harrison <mark.harrison@octopus.com>
2020-09-10 10:46:33 +0100

    AddLibraryVariableSetToSelectedProjects

--- Octopus.Client/PowerShell/Variables/AddLibraryVariableSetToSelectedProjects.ps1
+++ Octopus.Client/PowerShell/Variables/AddLibraryVariableSetToSelectedProjects.ps1
@@ -1,41 +1,38 @@
 # You can this dll from your Octopus Server/Tentacle installation directory or from
 # https://www.nuget.org/packages/Octopus.Client/
-Add-Type -Path 'Octopus.Client.dll'
- 
-$octopusProjectName = @('') # Name of the project(s) you want to add/remove the variable set
-$octopusVariableSetName = '' # !! CASE SENSATIVE !! Which variable set do you want to add/remove?
-$apikey = 'API-Key' # Get this from your profile
-$octopusURI = 'https://localhost' # Your server address
+# Load octopus.client assembly
+Add-Type -Path "c:\octopus.client\Octopus.Client.dll"
 
-$endpoint = New-Object Octopus.Client.OctopusServerEndpoint $octopusURI, $apikey 
+# Octopus variables
+$octopusURL = "https://youroctourl"
+$octopusAPIKey = "API-YOURAPIKEY"
+$spaceName = "default"
+$projectName = "MyProject"
+$libararySetName = "MyLibrarySet"
+
+$endpoint = New-Object Octopus.Client.OctopusServerEndpoint $octopusURL, $octopusAPIKey
 $repository = New-Object Octopus.Client.OctopusRepository $endpoint
+$client = New-Object Octopus.Client.OctopusClient $endpoint
 
-# Gets all existing variable sets from Octopus
-$allVariableSets = $repository.LibraryVariableSets.FindAll()
+try
+{
+    # Get space
+    $space = $repository.Spaces.FindByName($spaceName)
+    $repositoryForSpace = $client.ForSpace($space)
 
-# Gets the index of the variable set that we want to add
-$indexOfVariableSetIAmUsing = $allVariableSets.name.IndexOf($octopusVariableSetName)
+    # Get project
+    $project = $repositoryForSpace.Projects.FindByName($projectName)
 
-# Gets the object for the one variable set that we want
-$libraryVariableSet = New-Object Octopus.Client.Model.LibraryVariableSetResource
-$libraryVariableSet = $repository.LibraryVariableSets.Get($allVariableSets[$indexOfVariableSetIAmUsing].Id)
+    # Get library set
+    $librarySet = $repositoryForSpace.LibraryVariableSets.FindByName($libararySetName)
 
-# You can use this to add 1 new variable to the library set
-#$newVariable = new-object Octopus.Client.Model.VariableResource
-#$newVariable.Name = "newVariableName"
-#$newVariable.Value = "true"
- 
-#$variables = $repository.VariableSets.Get($libraryVariableSet.VariableSetId)
-#$variables.Variables.Add($newVariable)
-#$repository.VariableSets.Modify($variables)
+    # Add set to project
+    $project.IncludedLibraryVariableSetIds += $librarySet.Id
 
-# Find the project(s) you want
-foreach ($name in $octopusProjectName) {
-    $projects = $repository.Projects.FindByName($name)
-
-    # Add/Remove Variable Set for all specified projects
-    foreach ($project in $projects) {
-        $project.IncludedLibraryVariableSetIds.Add($libraryVariableSet.Id) #change to .Remove for removing the variable set/script module from all projects
-        $repository.Projects.Modify($project)
-    }
+    # Update project
+    $repositoryForSpace.Projects.Modify($project)
+}
+catch
+{
+    Write-Host $_.Exception.Message
 }
