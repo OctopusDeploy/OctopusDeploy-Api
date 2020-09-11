5ddd3bdc4671a3970133f42b05b1c462f775497d
Mark Harrison <mark.harrison@octopus.com>
2020-09-11 13:30:57 +0100

    Add ChangeFeed

---
+++ Octopus.Client/PowerShell/Feeds/ChangeFeed.ps1
@@ -0,0 +1,37 @@
+# You can get this dll from your Octopus Server/Tentacle installation directory or from
+# https://www.nuget.org/packages/Octopus.Client/
+Add-Type -Path "C:\Octo\Octopus.Client.dll"
+
+# Octopus variables
+$octopusURL = "https://youroctourl"
+$octopusAPIKey = "API-YOURAPIKEY"
+
+$spaceName = "default"
+$feedName = "nuget.org"
+$newFeedName = "nuget.org feed"
+
+$endpoint = New-Object Octopus.Client.OctopusServerEndpoint $octopusURL, $octopusAPIKey
+$repository = New-Object Octopus.Client.OctopusRepository $endpoint
+
+try
+{
+    # Get space id
+    $space = $repository.Spaces.FindByName($spaceName)
+    Write-Host "Using Space named $($space.Name) with id $($space.Id)"
+
+    # Create space specific repository
+    $repositoryForSpace = [Octopus.Client.OctopusRepositoryExtensions]::ForSpace($repository, $space)
+
+    # Get feed
+    $feed = $repositoryForSpace.Feeds.FindByName($feedName)
+
+    # Change feed name
+    $feed.Name = $newFeedName
+
+    # Update feed
+    $repositoryForSpace.Feeds.Modify($feed) | Out-Null
+}
+catch
+{
+    Write-Host $_.Exception.Message
+}
---
+++ REST/PowerShell/Feeds/ChangeFeed.ps1
@@ -0,0 +1,28 @@
+﻿# Define working variables
+$octopusURL = "https://youroctourl"
+$octopusAPIKey = "API-YOURAPIKEY"
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+$spaceName = "default"
+$feedName = "nuget.org"
+
+# Change property
+$newFeedName = "nuget.org feed"
+
+try
+{
+    # Get space
+    $space = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/spaces/all" -Headers $header) | Where-Object {$_.Name -eq $spaceName}
+
+    # Get feed
+    $feed = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/feeds/all" -Headers $header) | Where-Object {$_.Name -eq $feedName}
+    
+    # Change feed name
+    $feed.Name = $newFeedName
+    
+    # Update feed in Octopus
+    Invoke-RestMethod -Uri "$octopusURL/api/$($space.Id)/feeds/$($feed.Id)" -Body ($feed | ConvertTo-Json -Depth 10) -Headers $header -Method Put
+}
+catch
+{
+    Write-Host $_.Exception.Message
+}
