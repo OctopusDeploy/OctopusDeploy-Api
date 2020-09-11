df91a82e9583587d07ad4afe8006629a960d4b14
Michael Levan <mlevan1992@gmail.com>
2020-09-11 12:48:27 -0400

    Create a project group

---
+++ REST/Golang/Project-Groups/CreateProjectGroups/CreateProjectGroup.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	URL := os.Args[1]
+	apiKey := os.Args[2]
+
+	createProject(URL, apiKey)
+}
+
+func createProject(URL string, apiKey string) {
+
+	body, _ := json.Marshal(map[string]string{
+		"ProjectGroupId": "Projects-1",
+		"Name":           "testGoCodeeeee",
+		"LifecycleID":    "DevLifecycle",
+	})
+
+	put, err := http.NewRequest("POST", URL+"/api/projectgroup", bytes.NewBuffer(body))
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	put.Header.Set("X-Octopus-ApiKey", apiKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(put)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Response Status:", resp.Status)
+}
