e0745d1f32d58612c86a8a5daf91be8311f37ee1
Michael Levan <mlevan1992@gmail.com>
2020-09-11 11:21:50 -0400

    Create a project with Go

---
+++ REST/Golang/Projects/CreateProjects/createProjects.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	URL := os.Args[1]
+	apiKey := os.Args[2]
+
+	createProject(URL, apiKey)
+}
+
+func createProject(URL string, apiKey string) {
+
+	body, _ := json.Marshal(map[string]string{
+		"ProjectGroupId": "Projects-1",
+		"Name":           "testGoCode",
+		"LifecycleID":    "DevLifecycle",
+	})
+
+	put, err := http.NewRequest("POST", URL+"/api/projects", bytes.NewBuffer(body))
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	put.Header.Set("X-Octopus-ApiKey", apiKey)
+
+	client := &http.Client{}
+	resp, _ := client.Do(put)
+
+	fmt.Println(resp)
+}
