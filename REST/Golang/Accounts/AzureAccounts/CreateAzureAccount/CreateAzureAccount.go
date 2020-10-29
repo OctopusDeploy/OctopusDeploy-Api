c28119962999fcfa4ac5ed23c5e8b0c32526e2fa
Michael Levan <mlevan1992@gmail.com>
2020-10-29 14:02:56 -0400

    Azure account update with new client

--- REST/Golang/Accounts/AzureAccounts/CreateAzureAccount/CreateAzureAccount.go
+++ REST/Golang/Accounts/AzureAccounts/CreateAzureAccount/CreateAzureAccount.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/OctopusDeploy/go-octopusdeploy/client"
-	"github.com/OctopusDeploy/go-octopusdeploy/model"
+	"log"
+	"net/url"
+	"os"
+
+	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
 	"github.com/google/uuid"
 	"golang.org/x/crypto/ssh/terminal"
-	"log"
-	"os"
 )
 
 func main() {
-	octopusURL := os.Args[1]
+	octopusURL, _ := url.Parse(os.Args[1])
 	space := os.Args[2]
 	name := os.Args[3]
 	subscriptionId, _ := uuid.Parse(os.Args[4])
@@ -35,16 +36,17 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+
 	password := string(clientPassword)
-	azureClientPassword := model.NewSensitiveValue(password)
+	azureClientPassword := octopusdeploy.NewSensitiveValue(password)
 
 	// Call both functions from the main function
 	octopusAuth(octopusURL, APIKey, space)
 	CreateAzureAccount(octopusURL, APIKey, space, name, subscriptionId, tenantID, applicationID, azureClientPassword)
 }
 
-func octopusAuth(octopusURL, APIKey, space string) *client.Client {
-	client, err := client.NewClient(nil, octopusURL, APIKey, space)
+func octopusAuth(octopusURL *url.URL, APIKey, space string) *octopusdeploy.Client {
+	client, err := octopusdeploy.NewClient(nil, octopusURL, APIKey, space)
 	if err != nil {
 		log.Println(err)
 	}
@@ -52,15 +54,16 @@ func octopusAuth(octopusURL, APIKey, space string) *client.Client {
 	return client
 }
 
-func CreateAzureAccount(octopusURL string, APIKey string, space string, name string, subscriptionID uuid.UUID, tenantID uuid.UUID, applicationID uuid.UUID, azureClientPassword model.SensitiveValue) *model.Account {
+func CreateAzureAccount(octopusURL *url.URL, APIKey string, space string, name string, subscriptionID uuid.UUID, tenantID uuid.UUID, applicationID uuid.UUID, azureClientPassword *octopusdeploy.SensitiveValue) *octopusdeploy.AzureServicePrincipalAccount {
 	client := octopusAuth(octopusURL, APIKey, space)
-	Account, err := model.NewAzureServicePrincipalAccount(name, subscriptionID, tenantID, applicationID, azureClientPassword)
+	Account, err := octopusdeploy.NewAzureServicePrincipalAccount(name, subscriptionID, tenantID, applicationID, azureClientPassword)
 
 	if err != nil {
 		log.Println(err)
 	}
 
 	client.Accounts.Add(Account)
+	log.Printf("\nAccount %s: Created", name)
 
 	return Account
 }
--- REST/Golang/go.mod
+++ REST/Golang/go.mod
@@ -2,6 +2,7 @@ module main
 
 require (
 	github.com/OctopusDeploy/go-octopusdeploy v1.6.1-0.20201028054120-22a0acd9a3e6
+	github.com/google/uuid v1.1.2
 	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee
 )
 
