228201c677760f43ed0b17a1185f65a4a11c8709
octopusbob <bob.walker@octopus.com>
2020-12-01 09:43:17 -0600

    Rewrite create deployment with prompted variables

--- REST/PowerShell/Deployments/CreateDeploymentWithPromptVariables.ps1
+++ REST/PowerShell/Deployments/CreateDeploymentWithPromptVariables.ps1
@@ -4,42 +4,70 @@ $OctopusURL = "Your Octopus URL"
 
 $ProjectName = "Your Project Name"
 $EnvironmentName = "Your Environment Name"
+$ReleaseNumber = "Your Release Number"
+$spaceName = "Your Space Name"
+$promptedVariableValue = "VariableName::Variable Value"
 
 ##PROCESS##
 $Header =  @{ "X-Octopus-ApiKey" = $apiKey }
 
-#Getting Environment and Project By Name
-$Project = Invoke-WebRequest -Uri "$OctopusURL/api/projects/$ProjectName" -Headers $Header| ConvertFrom-Json
+$spaceList = Invoke-RestMethod "$OctopusUrl/api/spaces?partialName=$([System.Web.HTTPUtility]::UrlEncode($spaceName))&skip=0&take=1" -Headers $Header
+$spaceId = $spaceList.Items[0].Id
 
-$Environment = Invoke-WebRequest -Uri "$OctopusURL/api/Environments/all" -Headers $Header| ConvertFrom-Json
+$ProjectList = Invoke-RestMethod "$OctopusURL/api/$spaceId/projects?name=$([System.Web.HTTPUtility]::UrlEncode($projectName))&skip=0&take=1" -Headers $header
+$ProjectId = $ProjectList.Items[0].Id
 
-$Environment = $Environment | ?{$_.name -eq $EnvironmentName}
+$EnvironmentList = Invoke-RestMethod -Uri "$OctopusURL/api/$spaceId/Environments?name=$([System.Web.HTTPUtility]::UrlEncode($EnvironmentName))&skip=0&take=1" -Headers $Header
+$EnvironmentId = $EnvironmentList.Items[0].Id
 
-#Getting Deployment Template to get Next version 
-$dt = Invoke-WebRequest -Uri "$OctopusURL/api/deploymentprocesses/deploymentprocess-$($project.id)/template" -Headers $Header | ConvertFrom-Json 
+$ReleaseList = Invoke-RestMethod -Uri "$OctopusURL/api/$spaceId/projects/$ProjectId/releases?searchByVersion=$([System.Web.HTTPUtility]::UrlEncode($releaseNumber))&skip=0&take=1" -Headers $Header
+$ReleaseId = $ReleaseList.Items[0].Id
 
-#Creating Release
-$ReleaseBody =  @{ Projectid = $Project.Id
-            version = $dt.nextversionincrement } | ConvertTo-Json
+$deploymentPreview = Invoke-RestMethod "$OctopusUrl/api/$spaceId/releases/$releaseId/deployments/preview/$EnvironmentId?includeDisabledSteps=true" -Headers $Header
 
-$r = Invoke-WebRequest -Uri $OctopusURL/api/releases -Method Post -Headers $Header -Body $ReleaseBody | ConvertFrom-Json
-
-#Getting task to get preview
-$t = Invoke-WebRequest -Uri ($OctopusURL + $r.links.DeploymentTemplate) -Headers $Header | ConvertFrom-Json
-
-#Preview holds the variables labeles and names.
-$p = Invoke-WebRequest -Uri ($OctopusURL + $t.PromoteTo.links.Preview) -Headers $Header | ConvertFrom-Json
+$deploymentFormValues = @{}
+$promptedValueList = @(($promptedVariableValue -Split "`n").Trim())
+   
+foreach($element in $deploymentPreview.Form.Elements)
+{
+    $nameToSearchFor = $element.Control.Name
+    $uniqueName = $element.Name
+    $isRequired = $element.Control.Required
+    
+    $promptedVariablefound = $false
+    
+    Write-Host "Looking for the prompted variable value for $nameToSearchFor"
+    foreach ($promptedValue in $promptedValueList)
+    {
+        $splitValue = $promptedValue -Split "::"
+        Write-Host "Comparing $nameToSearchFor with provided prompted variable $($promptedValue[0])"
+        if ($splitValue.Length -gt 1)
+        {
+            if ($nameToSearchFor -eq $splitValue[0])
+            {
+                Write-Host "Found the prompted variable value $nameToSearchFor"
+                $deploymentFormValues[$uniqueName] = $splitValue[1]
+                $promptedVariableFound = $true
+                break
+            }
+        }
+    }
+    
+    if ($promptedVariableFound -eq $false -and $isRequired -eq $true)
+    {
+        Write-Highlight "Unable to find a value for the required prompted variable $nameToSearchFor, exiting"
+        Exit 1
+    }
+}
 
 #Creating deployment and setting value to the only prompt variable I have on $p.Form.Elements. You're gonna have to do some digging if you have more variables
 $DeploymentBody = @{ 
-            ReleaseID = $r.Id #mandatory
-            EnvironmentID = $Environment.id #mandatory
-            FormValues = @{ # Here is where you declare all your Prompt variables. Browse $p.Form.Elements to see which ones are available and add an extra line on this array for each one.
-                            "$($p.Form.Elements.name)" = "My Variable Value!"
-            ForcePackageDownload="False"
-            ForcePackageRedeployment="False"
-            UseGuidedFailure="False"
-                         }                             
+            ReleaseID = $releaseId #mandatory
+            EnvironmentID = $ReleaseId #mandatory
+            FormValues = $deploymentFormValues
+            ForcePackageDownload=$False
+            ForcePackageRedeployment=$False
+            UseGuidedFailure=$False
           } | ConvertTo-Json
           
-Invoke-WebRequest -Uri $OctopusURL/api/deployments -Method Post -Headers $Header -Body $DeploymentBody
+Invoke-RestMethod -Uri "$OctopusURL/api/$spaceId/deployments" -Method Post -Headers $Header -Body $DeploymentBody
