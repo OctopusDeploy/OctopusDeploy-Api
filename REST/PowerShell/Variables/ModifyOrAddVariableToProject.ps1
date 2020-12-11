39c2729603e7443acf32d52add7e42b5693cf168
Donny Bell <63249187+donnybell@users.noreply.github.com>
2020-12-11 16:18:19 -0500

    Now with environment scoping

--- REST/PowerShell/Variables/ModifyOrAddVariableToProject.ps1
+++ REST/PowerShell/Variables/ModifyOrAddVariableToProject.ps1
@@ -1,16 +1,14 @@
 $ErrorActionPreference = "Stop";
 
 # Define working variables
-$octopusURL = "https://youroctourl"
-$octopusAPIKey = "API-YOURAPIKEY"
+$octopusURL = "https://OctopusServerURL" # Octopus Server URL
+$octopusAPIKey = "API-XXXXXXXXXXXXXXXXXXXXXXXXXX" # API key goes here
+$projectName = "ProjectName" # Replace with your project name
+$spaceName = "Default" # Replace with the name of the space you are working in
+$environment = "DevEnv" # Replace with the name of the environment you want to scope the variables to
+
+# Defines header for API call
 $header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
-$projectName = "MyProject"
-$variable = @{
-    Name = "MyVariable"
-    Value = "MyValue"
-    Type = "String"
-    IsSensitive = $false
-}
 
 # Get space
 $space = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/spaces/all" -Headers $header) | Where-Object {$_.Name -eq $spaceName}
@@ -21,8 +19,26 @@ $project = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/pro
 # Get project variables
 $projectVariables = Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/variables/$($project.VariableSetId)" -Headers $header
 
-# Check to see if varialbe is already present
+# Get environment to scope to
+$environmentObj = $projectVariables.ScopeValues.Environments | Where { $_.Name -eq $environment } | Select -First 1
+
+# Define values for variable
+$variable = @{
+    Name = "VariableName" # Replace with a variable name
+    Value = "123456" # Replace with a value
+    Type = "String"
+    IsSensitive = $false
+    Scope = @{ 
+            Environment = @(
+                $environmentObj.Id
+                )
+            }
+}
+
+# Check to see if variable is already present
 $variableToUpdate = $projectVariables.Variables | Where-Object {$_.Name -eq $variable.Name}
+
+# If the variable does not exist, create it
 if ($null -eq $variableToUpdate)
 {
     # Create new object
@@ -31,6 +47,7 @@ if ($null -eq $variableToUpdate)
     $variableToUpdate | Add-Member -MemberType NoteProperty -Name "Value" -Value $variable.Value
     $variableToUpdate | Add-Member -MemberType NoteProperty -Name "Type" -Value $variable.Type
     $variableToUpdate | Add-Member -MemberType NoteProperty -Name "IsSensitive" -Value $variable.IsSensitive
+    $variableToUpdate | Add-Member -MemberType NoteProperty -Name "Scope" -Value $variable.Scope
 
     # Add to collection
     $projectVariables.Variables += $variableToUpdate
@@ -40,6 +57,7 @@ if ($null -eq $variableToUpdate)
 
 # Update the value
 $variableToUpdate.Value = $variable.Value
+$variableToUpdate.Scope = $variable.Scope
 
 # Update the collection
 Invoke-RestMethod -Method Put -Uri "$octopusURL/api/$($space.Id)/variables/$($project.VariableSetId)" -Headers $header -Body ($projectVariables | ConvertTo-Json -Depth 10)
