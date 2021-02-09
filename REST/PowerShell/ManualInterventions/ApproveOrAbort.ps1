7180105bdd651f292a3277a1facb0672e7fd9d3c
Mark Harrison <mark.harrison@octopus.com>
2021-02-09 12:41:06 +0000

    Update ApproveOrAbort

--- REST/PowerShell/ManualInterventions/ApproveOrAbort.ps1
+++ REST/PowerShell/ManualInterventions/ApproveOrAbort.ps1
@@ -1,16 +1,28 @@
-﻿$OctopusURL = "" #Octopus server URL
-$OctopusAPIKey = "" #Octopus API Key
-$InterruptionID = "" # i.e "Interruptions-204". You can get this ID from the deployment document like this -> /api/interruptions?regarding=[Deployment ID]
+﻿$ErrorActionPreference = "Stop";
+
+# Define working variables
+$octopusURL = "https://your.octopus.app"
+$octopusAPIKey = "API-YOURKEY"
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+$spaceName = "Default"
+
+# Get space Id
+$space = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/spaces/all" -Headers $header) | Where-Object {$_.Name -eq $spaceName}
+
+# e.g. "Interruptions-204". 
+# You can get this ID from the deployment document like this -> /api/interruptions?regarding=[Deployment ID]
+$InterruptionID = "Interruptions-145" 
 
 $header = @{ "X-Octopus-ApiKey" = $OctopusAPIKey }
 
-$body = @{Instructions= ""
-            Notes = "Message" #message 
-            Result="Proceed" #Set this property to "Abort" to abort the deployment           
+$body = @{Instructions= $null
+            Notes = "Message" 
+            # If you wish to Exclude the machine from the deployment (in case of a rolling deploy), uncomment the line below.
+            # Guidance = "Exclude" 
         } | ConvertTo-Json
 
-#Take responsability for the Intervention
-Invoke-RestMethod "$OctopusURL/api/interruptions/$InterruptionID/responsible" -Method Put -Headers $header
+# Take responsibility for the Intervention
+Invoke-RestMethod "$OctopusURL/api/$($space.Id)/interruptions/$InterruptionID/responsible" -Method Put -Headers $header
 
 #Approve/abort the intervention
-Invoke-RestMethod "$OctopusURL/api/interruptions/$InterruptionID/submit" -Body $body -Method Post -Headers $header
+Invoke-RestMethod "$OctopusURL/api/$($space.Id)/interruptions/$InterruptionID/submit" -Body $body -Method Post -Headers $header
