7539effceb1d5c06735510e6aad389895acd68e0
Mark Harrison <mark.harrison@octopus.com>
2021-02-09 17:30:55 +0000

    Add environments to space

---
+++ REST/PowerShell/Environments/AddEnvironments.ps1
@@ -0,0 +1,31 @@
+$ErrorActionPreference = "Stop";
+
+# Define working variables
+$octopusURL = "https://youroctopus.octopus.app"
+$octopusAPIKey = "API-YOURAPIKEY"
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+
+$spaceName = "API Playground"
+$environments = @("Development", "Test", "Staging", "Production")
+
+# Get space
+$spaces = Invoke-RestMethod -Uri "$octopusURL/api/spaces?partialName=$([uri]::EscapeDataString($spaceName))&skip=0&take=100" -Headers $header 
+$space = $spaces.Items | Where-Object { $_.Name -eq $spaceName }
+
+foreach ($environment in $environments) {
+    $body = @{
+        Name = $environment
+    } | ConvertTo-Json
+
+    # Check to see if environment exists
+    $environments = Invoke-RestMethod -Uri "$octopusURL/api/$($space.Id)/environments?partialName=$([uri]::EscapeDataString($environment))&skip=0&take=100" -Headers $header
+    $existingEnvironment = $environments.Items | Where-Object { $_.Name -eq $environment }
+    if($null -ne $existingEnvironment) {
+        Write-Host "Environment $environment' already exists. Nothing to create :)"
+    }
+    else {
+        Write-Host "Creating environment '$environment'"
+        $response = Invoke-RestMethod -Uri "$octopusURL/api/$($space.Id)/environments" -Headers $header -Method Post -Body $body 
+        Write-Host "EnvironmentId: $($response.Id)"
+    }
+}
