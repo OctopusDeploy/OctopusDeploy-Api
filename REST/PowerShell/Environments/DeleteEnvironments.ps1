6fb6eaaa24902672bc30df9b0f5508ec523e703a
Mark Harrison <mark.harrison@octopus.com>
2021-02-17 12:38:34 +0000

    Update script

--- REST/PowerShell/Environments/DeleteEnvironments.ps1
+++ REST/PowerShell/Environments/DeleteEnvironments.ps1
@@ -14,12 +14,15 @@ $spaces = Invoke-RestMethod -Uri "$octopusURL/api/spaces?partialName=$([uri]::Es
 $space = $spaces.Items | Where-Object { $_.Name -eq $spaceName }
 
 if($deleteAllEnvironments -eq $True) {
-    Write-Warning "Querying all environments in Space: $spaceName"
-    $environmentsResource = Invoke-RestMethod -Uri "$octopusURL/api/$($space.Id)/environments?skip=0&take=100000" -Headers $header
-    $environmentNames = $environmentsResource.Items | Select-Object "$($_.Name)"
+    Write-Host "Querying all environments in Space: $spaceName"
+    $environmentsResource = Invoke-RestMethod -Uri "$octopusURL/api/$($space.Id)/environments?skip=0&take=100000" -Headers $header   
+    $environmentNames = $environmentsResource.Items | ForEach-Object { "$($_.Name)" }
 }
 
+Write-Host "Found $($environmentNames.Length) environments"
+
 foreach ($environment in $environmentNames) {
+    
     # Check to see if environment exists
     $environments = Invoke-RestMethod -Uri "$octopusURL/api/$($space.Id)/environments?partialName=$([uri]::EscapeDataString($environment))&skip=0&take=100" -Headers $header
     $existingEnvironment = $environments.Items | Where-Object { $_.Name -eq $environment }
@@ -28,6 +31,7 @@ foreach ($environment in $environmentNames) {
         Write-Host "Environment '$environment' doesnt exists. Nothing to delete :)"
     }
     else {
+
         Write-Host "Deleting environment '$environment'"
         Invoke-RestMethod -Uri "$octopusURL/api/$($space.Id)/environments/$($existingEnvironment.Id)" -Headers $header -Method Delete -Body $body 
     }
