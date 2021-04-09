cc12f50c9c564a5217feb67309825d6e644b0cf7
Mark Harrison <mark.harrison@octopus.com>
2021-04-09 11:15:02 +0100

    Update tenant variable update to support sensitive control types

--- REST/PowerShell/Tenants/UpdateMultipleTenantVariablesWithSingleValue.ps1
+++ REST/PowerShell/Tenants/UpdateMultipleTenantVariablesWithSingleValue.ps1
@@ -9,6 +9,7 @@ $spaceName = "Default" # Name of the Space
 $tenantName = "TenantName" # The tenant name
 $variableTemplateName = "ProjectTemplateName" # Choose the template Name
 $newValue = "NewValue" # Choose a new variable value, assumes same per environment
+$NewValueIsBoundToOctopusVariable=$True # Choose $True if the $newValue is an Octopus variable e.g. #{SomeValue}
 
 # Get space
 $space = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/spaces/all" -Headers $header) | Where-Object {$_.Name -eq $spaceName}
@@ -32,36 +33,49 @@ foreach ($projectKey in $projects)
     Write-Host "Working on Project: $projectName ($projectKey)"
 
     # Get Project template ID
-    $variableTemplateId = ($project.Templates | Where-Object Name -eq $variableTemplateName | Select-Object -First 1).Id
+    $variableTemplate = ($project.Templates | Where-Object Name -eq $variableTemplateName | Select-Object -First 1)
     
-    if($null -ne $variableTemplateId) {
+    
+    if($null -ne $variableTemplate) {
+
+        $variableTemplateId = $variableTemplate.Id
+        $variableTemplateIsSensitiveControlType = $variableTemplate.DisplaySettings.{Octopus.ControlType} -eq "Sensitive"
 
         Write-Host "Found templateId for Template: $variableTemplateName = $variableTemplateId"
         $projectConnectedEnvironments = $project.Variables | Get-Member | Where-Object {$_.MemberType -eq "NoteProperty"} | Select-Object -ExpandProperty "Name"
 
         # Loop through each of the connected environments variables
         foreach($envKey in $projectConnectedEnvironments) {
-            
-            # Find variable which matches the current connected environment
-            $currentValue = $project.Variables.$envKey.$variableTemplateId
-            
-            # If null / only default value exists, add new value in 
-            if($null -eq $currentValue ) {
-                Write-Host "No value found for $variableTemplateName, adding in new value = $newValue for Environment '$envKey' "
+            # Check for Environment project template entry, and add if not present
+            if($null -eq $project.Variables.$envKey.$variableTemplateId) {
                 $project.Variables.$envKey | Add-Member -MemberType NoteProperty -Name $variableTemplateId -Value $newValue
+            }
+
+            # Check sensitive control types differently
+            if($variableTemplateIsSensitiveControlType -eq $True) {
+                # If $newValue denotes an octopus variable e.g. #{SomeVar}, treat it as if it were text
+                if($NewValueIsBoundToOctopusVariable -eq $True) {      
+                    Write-Host "Adding in new text value (treating as octopus variable) in Environment '$envKey' for $variableTemplateName"             
+                    $project.Variables.$envKey.$variableTemplateId = $newValue
+                }    
+                else {
+                    $newSensitiveValue = [PsCustomObject]@{
+                        HasValue = $True
+                        NewValue = $newValue
+                    }
+                    Write-Host "Adding in new sensitive value = '********' in Environment '$envKey' for $variableTemplateName"
+                    $project.Variables.$envKey.$variableTemplateId = $newSensitiveValue
+                }
             } 
             else {
-                # Get Current value
-                Write-Host "Found $variableTemplateName in Environment '$envKey' with Value = $currentValue"
-                # Set the new value for this connected environment
+                Write-Host "Adding in new value = $newValue in Environment '$envKey' for $variableTemplateName"
                 $project.Variables.$envKey.$variableTemplateId = $newValue
-            }        
+            }       
         }
     }
     else {
         Write-Host "Couldnt find project template: $variableTemplateName for project $projectName"
     }
-
 }
 # Update the variables with the new value
 Invoke-RestMethod -Method Put -Uri "$octopusURL/api/$($space.Id)/tenants/$($tenant.Id)/variables" -Headers $header -Body ($variables | ConvertTo-Json -Depth 10)
