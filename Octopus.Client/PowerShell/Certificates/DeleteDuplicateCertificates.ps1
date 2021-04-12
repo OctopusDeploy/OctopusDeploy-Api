fed158f3287468cb38bc9b7aa5eaecbc73f4933d
Daniel Fischer <daniel.fischer@octopus.com>
2021-04-12 16:27:39 +1000

    Create DeleteDuplicateCertificates.ps1
    Will delete duplicate certificates stored in Octopus based on SerialNumber. The first of each cert it finds with a unique SerialNumber will be added to a list. All others will be deleted.

---
+++ Octopus.Client/PowerShell/Certificates/DeleteDuplicateCertificates.ps1
@@ -0,0 +1,30 @@
+Add-Type -Path 'path\to\Octopus.Client.dll' 
+
+# Create endpoint and client
+$endpoint = New-Object Octopus.Client.OctopusServerEndpoint("http://OctopusServer/", "API-KEY")
+$client = New-Object Octopus.Client.OctopusClient($endpoint)
+
+# Get default repository and get space by name
+#$repository = $client.ForSystem()
+#$space = $repository.Spaces.FindByName("Second")
+
+# Get space specific repository and get all projects in space
+#$repo = $client.ForSpace($space)
+
+$certificates = $client.Repository.Certificates.GetAll()
+
+$certsToKeep = New-Object Collections.Generic.List[string]
+
+# Will remove any duplicate certificates from Octopus based on SerialNumber. Delete function is commented out for a dry run. 
+
+foreach ($cert in $certificates) {
+
+    if ($certsToKeep -contains $cert.SerialNumber) {
+    Write-host "Deleting:" $cert.id
+    #$client.Repository.Certificates.Delete($cert) # Run this script before you enable the delete to confirm that you are Keeping & Deleting the correct certificates.
+    }
+    else {
+        $certsToKeep.Add($cert.SerialNumber)
+        Write-Host "Keeping:" $cert.id  
+    }
+}
