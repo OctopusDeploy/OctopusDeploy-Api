d47a46922dab2044599ebee102be94d8e0606f2e
octopusbob <bob.walker@octopus.com>
2021-04-26 09:19:07 -0500

    Added script to find the last release in a specific environment, update variables, then deploy a new release to the destination environment

---
+++ REST/PowerShell/Deployments/UpdateVariablesThenRedeploy.ps1
@@ -0,0 +1,164 @@
+$octopusUrl = "https://YOUR URL" 
+$apiKey = "YOUR API KEY"
+$projectName = "YOUR PROJECT NAME"
+$sourceEnvironmentName = "Staging" 
+$destinationEnvironmentName = "Test"
+$spaceName = "YOUR SPACE NAME"
+$variableNameToUpdate = "MyVariableToFind"
+$newValue = "Updated Value"
+
+function Invoke-OctopusApi
+{
+    param
+    (
+        $octopusUrl,
+        $endPoint,
+        $spaceId,
+        $apiKey,
+        $method,
+        $item     
+    )
+
+    if ([string]::IsNullOrWhiteSpace($SpaceId))
+    {
+        $url = "$OctopusUrl/api/$EndPoint"
+    }
+    else
+    {
+        $url = "$OctopusUrl/api/$spaceId/$EndPoint"    
+    }  
+
+    try
+    {        
+        if ($null -ne $item)
+        {
+            $body = $item | ConvertTo-Json -Depth 10
+            Write-Verbose $body
+
+            Write-Host "Invoking $method $url"
+            return Invoke-RestMethod -Method $method -Uri $url -Headers @{"X-Octopus-ApiKey" = "$ApiKey" } -Body $body -ContentType 'application/json; charset=utf-8' 
+        }
+
+        Write-Host "No data to post or put, calling bog standard invoke-restmethod for $url"
+        $result = Invoke-RestMethod -Method $method -Uri $url -Headers @{"X-Octopus-ApiKey" = "$ApiKey" } -ContentType 'application/json; charset=utf-8'
+
+        return $result               
+    }
+    catch
+    {
+        if ($null -ne $_.Exception.Response)
+        {
+            if ($_.Exception.Response.StatusCode -eq 401)
+            {
+                Write-Error "Unauthorized error returned from $url, please verify API key and try again"
+            }
+            elseif ($_.Exception.Response.statusCode -eq 403)
+            {
+                Write-Error "Forbidden error returned from $url, please verify API key and try again"
+            }
+            else
+            {                
+                Write-Host -Message "Error calling $url $($_.Exception.Message) StatusCode: $($_.Exception.Response.StatusCode )"
+            }            
+        }
+        else
+        {
+            Write-Host $_.Exception
+        }
+    }
+
+    Throw "There was an error calling the Octopus API please check the log for more details"
+}
+
+$spaceList = Invoke-OctopusApi -octopusUrl $octopusUrl -apiKey $apiKey -method "GET" -spaceId $null -item $null -endPoint "spaces?partialName=$([uri]::EscapeDataString($spaceName))&skip=0&take=100"
+$space = $spaceList.Items | Where-Object {$_.Name -eq $spaceName}
+$spaceId = $space.Id
+Write-Host "The space id for space name $spaceName is $spaceId"
+
+$sourceEnvironmentList = Invoke-OctopusApi -octopusUrl $octopusUrl -apiKey $apiKey -method "GET" -spaceId $spaceId -item $null -endPoint "environments?partialName=$([uri]::EscapeDataString($sourceEnvironmentName))&skip=0&take=100"
+$sourceEnvironment = $sourceEnvironmentList.Items | Where-Object {$_.Name -eq $sourceEnvironmentName}
+$sourceEnvironmentId = $sourceEnvironment.Id
+Write-Host "The environment id for environment name $sourceEnvironmentName is $sourceEnvironmentId"
+
+$destinationEnvironmentList = Invoke-OctopusApi -octopusUrl $octopusUrl -apiKey $apiKey -method "GET" -spaceId $spaceId -item $null -endPoint "environments?partialName=$([uri]::EscapeDataString($destinationEnvironmentName))&skip=0&take=100"
+$destinationEnvironment = $destinationEnvironmentList.Items | Where-Object {$_.Name -eq $destinationEnvironmentName}
+$destinationEnvironmentId = $destinationEnvironment.Id
+Write-Host "The environment id for environment name $destinationEnvironmentName is $destinationEnvironmentId"
+
+$projectList = Invoke-OctopusApi -octopusUrl $octopusUrl -apiKey $apiKey -method "GET" -spaceId $spaceId -item $null -endPoint "projects?partialName=$([uri]::EscapeDataString($projectName))&skip=0&take=100"
+$project = $projectList.Items | Where-Object {$_.Name -eq $projectName}
+$projectId = $project.Id
+Write-Host "The project id for project name $projectName is $projectId"
+
+Write-Host "I have all the Ids I need, I am going to find the deployment now"
+$taskList = Invoke-OctopusApi -octopusUrl $octopusUrl -apiKey $apiKey -method "GET" -spaceId $null -item $null -endPoint "tasks?skip=0&environment=$($sourceEnvironmentId)&project=$($projectId)&name=Deploy&states=Success&spaces=$spaceId&includeSystem=false"
+if ($taskList.Items.Count -eq 0)
+{
+    Write-Host "Unable to find a successful deployment for $projectName to $sourceEnvironmentName"
+    exit 0
+}
+
+$lastDeploymentTask = $taskList.Items[0]
+$deploymentId = $lastDeploymentTask.Arguments.DeploymentId
+Write-Host "The id of the last deployment for $projectName to $sourceEnvironmentName is $deploymentId"
+
+$deploymentDetails = Invoke-OctopusApi -octopusUrl $octopusUrl -apiKey $apiKey -method "GET" -spaceId $spaceId -item $null -endPoint "deployments/$deploymentId"
+$releaseId = $deploymentDetails.ReleaseId
+Write-Host "The release id for $deploymentId is $releaseId"
+
+$releaseDetails = Invoke-OctopusApi -octopusUrl $octopusUrl -apiKey $apiKey -method "GET" -spaceId $spaceId -item $null -endPoint "releases/$releaseId"
+Write-Host "The version number of the most recent release is $($releaseDetails.Version)"
+
+Write-Host "I now have the release I am looking for, time to update the variable value"
+
+$variableSetValues = Invoke-OctopusApi -octopusUrl $octopusUrl -apiKey $apiKey -method "GET" -spaceId $spaceId -item $null -endPoint "variables/$($project.VariableSetId)"
+$variableFound = $false
+foreach ($variable in $variableSetValues.Variables)
+{
+    if ($variable.Name -ne $variableNameToUpdate)
+    {
+        continue
+    }
+
+    ## Replace with your own matching logic
+    if ($variable.Scope.Environment -contains $destinationEnvironmentId)
+    {
+        Write-Host "Variable to update found!"
+        $variable.Value = $newValue
+        $variableFound = $true
+        break
+    }
+}
+
+if ($variableFound -eq $false)
+{
+    Write-Host "Unable to find the variable to update, exiting"
+    exit 0
+}
+Write-Host "Updating the variable set values"
+$updatedVariableSetValues = Invoke-OctopusApi -octopusUrl $octopusUrl -apiKey $apiKey -method "PUT" -spaceId $spaceId -item $variableSetValues -endPoint "variables/$($project.VariableSetId)"
+
+$newRelease = @{
+    ChannelId = $releaseDetails.ChannelId
+    ProjectId = $releaseDetails.ProjectId
+    ReleaseNotes = $releaseDetails.ReleaseNotes
+    SelectedPackages = $releaseDetails.SelectedPackages
+    Version = "$($releaseDetails.Version)-UpdatedVariables"
+}
+$octopusReleaseObject = Invoke-OctopusApi -octopusUrl $octopusUrl -apiKey $apiKey -method "POST" -spaceId $spaceId -item $newRelease -endPoint "releases"
+
+$newDeployment = @{
+    EnvironmentId = $destinationEnvironmentId
+    ReleaseId = $octopusReleaseObject.ReleaseId
+    ExcludedMachines = @()
+    ForcePackageDownload = $false
+    ForcePackageRedeployment = $false
+    FormValue = @{}
+    QueueTime = $null
+    QueueTimeExpiry = $null
+    SkipActions = @()
+    SpecificMachineIds = @()
+    TenantId = $null
+    UseGuidedFailure = $false
+}
+$newDeployment = Invoke-OctopusApi -octopusUrl $octopusurl -apiKey $apiKey -method "POST" -spaceId $spaceId -item $newDeployment -endPoint "deployments"
