f513f42a319ba65887aaf3f8e91280633e7c3075
Mark Harrison <mark.harrison@octopus.com>
2021-07-22 13:17:59 +0100

    Update create and publish runbook example

--- Octopus.Client/Csharp/Runbooks/CreateAndPublishRunbookSnapshot.cs
+++ Octopus.Client/Csharp/Runbooks/CreateAndPublishRunbookSnapshot.cs
@@ -1,83 +1,59 @@
 // If using .net Core, be sure to add the NuGet package of System.Security.Permissions
 
-// Reference Octopus.Client
 //#r "path\to\Octopus.Client.dll"
 
 using Octopus.Client;
 using Octopus.Client.Model;
+using System;
+using System.Linq;
 
-// Declare working varibles
-var octopusURL = "https://youroctourl";
-var octopusAPIKey = "API-YOURAPIKEY";
-string spaceName = "default";
-string projectName = "MyProject";
-string runbookName = "MyRunbook";
+// If using .net Core, be sure to add the NuGet package of System.Security.Permissions
+
+var octopusURL = "https://YourURL";
+var octopusAPIKey = "API-YourAPIKey";
+var spaceName = "Default";
+var projectName = "MyProject";
+var runbookName = "MyRunbook";
 
 // Create repository object
 var endpoint = new OctopusServerEndpoint(octopusURL, octopusAPIKey);
 var repository = new OctopusRepository(endpoint);
 var client = new OctopusClient(endpoint);
 
-try
+// Get space
+var space = repository.Spaces.FindByName(spaceName);
+var spaceRepository = client.ForSpace(space);
+
+// Get project
+var project = spaceRepository.Projects.FindByName(projectName);
+
+// Get runbook
+var runbook = spaceRepository.Runbooks.FindByName(project, runbookName);
+
+// Get runbook snapshot tempalte
+var runbookSnapshotTemplate = spaceRepository.Runbooks.GetRunbookSnapshotTemplate(runbook);
+
+// Create runbook snapshot
+var runbookSnapshot = new Octopus.Client.Model.RunbookSnapshotResource(project.Id);
+runbookSnapshot.RunbookId = runbook.Id;
+runbookSnapshot.Name = runbookSnapshotTemplate.NextNameIncrement;
+runbookSnapshot.SpaceId = space.Id;
+
+// Add any referenced packages
+foreach (var package in runbookSnapshotTemplate.Packages)
 {
-    // Get space
-    var space = repository.Spaces.FindByName(spaceName);
-    var repositoryForSpace = client.ForSpace(space);
+    // Get the feed
+    var feed = spaceRepository.Feeds.Get(package.FeedId);
+    var latestPackage = spaceRepository.Feeds.GetVersions(feed, (new string[] { package.PackageId }));
 
-    // Get project
-    var project = repositoryForSpace.Projects.FindByName(projectName);
+    // Create new selected package object
+    var selectedPackage = new Octopus.Client.Model.SelectedPackage(package.ActionName, package.PackageReferenceName, latestPackage[0].Version);
 
-    // Get runbook
-    var runbook = repositoryForSpace.Runbooks.FindByName(project, runbookName);
-
-    // Get runbook process
-    var runbookProcess = repositoryForSpace.RunbookProcesses.Get(runbook.RunbookProcessId);
-
-    // Get runbook snapshot template
-    var runbookSnapshotTemplate = repositoryForSpace.RunbookProcesses.GetTemplate(runbookProcess);
-
-    // Create a runbook snapshot
-    var runbookSnapshot = new RunbookSnapshotResource
-    {
-        ProjectId = project.Id,
-        RunbookId = runbook.Id,
-        Name = runbookSnapshotTemplate.NextNameIncrement,
-        // Add optional notes next
-        Notes = null,
-        SelectedPackages = new List<Octopus.Client.Model.SelectedPackage>()
-    };
-
-    // Include latest built-in feed packages
-    foreach (var package in runbookSnapshotTemplate.Packages)
-    {
-        if (package.FeedId == "feeds-builtin")
-        {
-            // Get latest package version
-            var packages = repositoryForSpace.BuiltInPackageRepository.ListPackages(package.PackageId, take: 1);
-            var latestPackage = packages.Items.FirstOrDefault();
-            if (latestPackage == null)
-            {
-                throw new Exception("Couldnt find latest package for " + package.PackageId);
-            }
-
-            runbookSnapshot.SelectedPackages.Add(new SelectedPackage { ActionName = package.ActionName, Version = latestPackage.Version, PackageReferenceName = package.PackageReferenceName });
-        }
-    }
-
-    // Create new snapshot
-    var runbookPublishedSnapshot = repositoryForSpace.RunbookSnapshots.Create(runbookSnapshot, new { publish = true });
-
-    // Re-retrieve runbook
-    runbook = repositoryForSpace.Runbooks.Get(runbook.Id);
-
-    // Assign the snapshot as the published one.
-    runbook.PublishedRunbookSnapshotId = runbookPublishedSnapshot.Id;
-    repositoryForSpace.Runbooks.Modify(runbook);
-    Console.WriteLine("Published runbook snapshot: {0} ({1})", runbookPublishedSnapshot.Id, runbookPublishedSnapshot.Name);
+    // Add to runbook snapshot
+    runbookSnapshot.SelectedPackages.Add(selectedPackage);
 }
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-    Console.ReadLine();
-    return;
-}
+
+// Publish snapshot
+runbookSnapshot = spaceRepository.RunbookSnapshots.Create(runbookSnapshot);
+runbook.PublishedRunbookSnapshotId = runbookSnapshot.Id;
+spaceRepository.Runbooks.Modify(runbook); 
