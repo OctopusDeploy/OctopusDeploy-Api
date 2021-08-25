59aa2ae9486f5d0a161c5539822d1c509f4dab35
Mark Harrison <mark.harrison@octopus.com>
2021-08-25 15:50:41 +0100

    Update more usages of Spaces.GetByName in Go examples

--- REST/Golang/Accounts/AWSAccounts/CreateAWSAccount/CreateAWSAccount.go
+++ REST/Golang/Accounts/AWSAccounts/CreateAWSAccount/CreateAWSAccount.go
@@ -353,14 +353,22 @@ func octopusAuth(octopusURL *url.URL, APIKey, space string) *octopusdeploy.Clien
 func GetSpace(octopusURL *url.URL, APIKey string, spaceName string) *octopusdeploy.Space {
 	client := octopusAuth(octopusURL, APIKey, "")
 
+	spaceQuery := octopusdeploy.SpacesQuery{
+		Name: spaceName,
+	}
+
 	// Get specific space object
-	space, err := client.Spaces.GetByName(spaceName)
+	spaces, err := client.Spaces.Get(spaceQuery)
 
 	if err != nil {
 		log.Println(err)
-	} else {
-		fmt.Println("Retrieved space " + space.Name)
 	}
 
-	return space
+	for _, space := range spaces.Items {
+		if space.Name == spaceName {
+			return space
+		}
+	}
+
+	return nil
 }
