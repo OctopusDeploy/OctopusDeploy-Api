b922e88fb7eec67c3705122c2e9f480ce3e26f43
briggs-octo <104870596+briggs-octo@users.noreply.github.com>
2022-07-06 09:23:54 -0700

    Adding Octopus.com user invite script

---
+++ REST/bash/Users/inviteUser.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Bash script to programatically invite a new user via Octopus.com
+
+# Cookie and token variables can be acquired by inspecting a user invite web request from Octopus.com and copying as a curl request.
+
+# User roles: 0 = Billing, 1 = Tech, 2 = Administrator
+
+OCTOPUS_ORG="Organizations-xxx"
+COOKIE=""
+RV_TOKEN=""
+NEW_USER_EMAIL="user%40domain.com"
+NEW_USER_ROLE="0"
+
+curl "https://octopus.com/invitation/$OCTOPUS_ORG/invite" \
+  -H 'content-type: application/x-www-form-urlencoded' \
+  -H "cookie: $COOKIE" \
+  --data-raw "EmailAddress=$NEW_USER_EMAIL&role=$NEW_USER_ROLE&__RequestVerificationToken=$RV_TOKEN"
