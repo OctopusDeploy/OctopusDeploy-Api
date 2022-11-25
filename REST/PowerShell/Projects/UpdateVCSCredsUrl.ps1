38c2ac76f89c46645411fdea9040e7ab588105c5
patrick-smergut-octopus <83607909+patrick-smergut-octopus@users.noreply.github.com>
2022-11-25 10:58:05 -0800

    Standardize input variables

--- REST/PowerShell/Projects/UpdateVCSCredsUrl.ps1
+++ REST/PowerShell/Projects/UpdateVCSCredsUrl.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop";
 
 # Define working variables
-$OCTOPUS_API_BASE_URL = "https://youroctourl"
+$octopusURL = "https://youroctourl"
 $octopusAPIKey = "API-####"
 $header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
 $projectName = "Project Name"
@@ -10,7 +10,7 @@ $gitCredId = "GitCredentials-##" # From Library > Git Credentials
 $gitRepoUrl = "https://github.com/<user>/<repo>.git"
 
 # Get project
-$project = (Invoke-RestMethod -Method Get -Uri "$OCTOPUS_API_BASE_URL/api/$($spaceId)/projects/all" -Headers $header) | Where-Object {$_.Name -eq $projectName}
+$project = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($spaceId)/projects/all" -Headers $header) | Where-Object {$_.Name -eq $projectName}
 
 # Change from Username/Password at the project level to a Git Credential in the library (optional, but Git Creds are much easier - can change back via UI if desired)
 $project.PersistenceSettings.Credentials.Type = "Reference"
@@ -22,4 +22,4 @@ $project.PersistenceSettings.Credentials | Add-Member -NotePropertyName Id -Note
 $project.PersistenceSettings.Url = $gitRepoUrl
 
 # Commit changes to Octopus project
-Invoke-RestMethod -Method Put -Uri "$OCTOPUS_API_BASE_URL/api/$($spaceId)/projects/$($project.Id)" -Headers $header -Body ($project | ConvertTo-Json -Depth 100)
+Invoke-RestMethod -Method Put -Uri "$octopusURL/api/$($spaceId)/projects/$($project.Id)" -Headers $header -Body ($project | ConvertTo-Json -Depth 100)
