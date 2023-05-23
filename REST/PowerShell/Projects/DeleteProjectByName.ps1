43d99f21c087fd5d24c3d59253239d07f323a9d1
Donny Bell <63249187+donnybell@users.noreply.github.com>
2023-05-23 17:41:02 +0100

    Update DeleteProjectByName.ps1
    Updated with:
    - Prompted parameters
    - Informational Try/Catch
    - Confirmation before Project deletion when prompts are enabled

--- REST/PowerShell/Projects/DeleteProjectByName.ps1
+++ REST/PowerShell/Projects/DeleteProjectByName.ps1
@@ -1,17 +1,70 @@
-﻿$ErrorActionPreference = "Stop";
+# ==================================================================================================================
+# This script deletes a Project on the specified Octopus Server.
+# ==================================================================================================================
 
-# Define working variables
-$octopusURL = "https://youroctourl"
-$octopusAPIKey = "API-YOURAPIKEY"
-$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
-$spaceName = "Default"
-$projectName = "MyProject"
+$ErrorActionPreference = "Stop";
 
-# Get space
-$space = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/spaces/all" -Headers $header) | Where-Object {$_.Name -eq $spaceName}
+# ====== BYPASS PROMPTS? ======
+$BypassPrompts = $false # Set to $true if you wish to predefine your parameters
 
-# Get project
-$project = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/projects/all" -Headers $header) | Where-Object {$_.Name -eq $projectName}
 
-# Delete project
-Invoke-RestMethod -Method Delete -Uri "$octopusURL/api/$($space.Id)/projects/$($project.Id)" -Headers $header
+# ====== PARAMETERS ======
+If ($BypassPrompts) {
+	
+	# === Predefined Parameters (Optional) ===
+    $OctopusURL = "http://YOUR_OCTOPUS_URL.bla"
+    $OctopusAPIKey = "API-XXXXXXXXXXXXXXXXXX"
+    $SpaceId = "Spaces-XX"
+    $ProjectName = "My Project"
+}
+
+If (!$BypassPrompts) {
+	
+	# === Prompted Parameters ===
+	$OctopusAPIKey = (Read-Host "Enter your Octopus API key (example: `"API-XXXXXXXXXXXXXXXXXX`")").trim('"')
+	$OctopusURL = (Read-Host "Enter your Octopus Instance URL with no trailing slash (i.e. `"http://YOUR_OCTOPUS_URL.bla`")").trim('"')
+	$SpaceId = (Read-Host "Enter the SpaceId where the Library Variable Set resides (example: `"Spaces-1`")").trim('"')
+	$ProjectName = (Read-Host "Enter a name for your new Project (example: `"My Project`")").trim('"')
+}
+
+$Header = @{ "X-Octopus-ApiKey" = $OctopusAPIKey }
+
+# ====== SCRIPT BODY ======
+# Try to GET the ProjectId for $ProjectName
+Try {
+    $Project = (Invoke-RestMethod -Method GET "$($OctopusURL)/api/$($SpaceId)/Projects/all" -Headers $Header) | Where-Object {$_.Name -eq $ProjectName}
+    If (!$Project) {throw}
+}
+Catch {
+    Write-Warning "Unable to find a ProjectId for the Project Name `"$($ProjectName)`" via `"$($OctopusURL)/api/$($SpaceId)/Projects/all`""
+    Write-Warning "Check your parameters (Octopus API key, URL, SpaceId, etc.), ensure your API key has sufficient permissions, and the Octopus Server is accessible from this machine."
+    break
+}
+
+# Confirmation (ignored if $BypassPrompts = $true)
+If (!$BypassPrompts) {
+    Write-Host "=================================================================================================================================="
+    $Confirm = (Read-Host "Are you sure you want to DELETE the Project `"$ProjectName`" ($($Project.Id))? This cannot be undone. (Type Y to continue or N to quit)").trim('"')
+	While (($Confirm -ne "N") -and ($Confirm -ne "Y")) {
+		$Confirm = (Read-Host "Are you sure you want to DELETE the Project `"$ProjectName`" ($($Project.Id))? This cannot be undone. (Type Y to continue or N to quit)").trim('"')
+	}  
+	If ($Confirm -eq "N") {
+		Write-Warning "Aborted. No changes were made."
+		break
+	}
+}
+
+# Delete Project
+Invoke-RestMethod -Method DEL "$($OctopusURL)/api/$($SpaceId)/Projects/$($Project.Id)" -Headers $Header
+Try {
+    $DeleteCheck = (Invoke-RestMethod -Method GET "$($OctopusURL)/api/$($SpaceId)/Projects/all" -Headers $Header) | Where-Object {$_.Name -eq $ProjectName}
+    If (!$DeleteCheck) {
+        Write-Host "The Project named `"$($ProjectName)`" ($($Project.Id)) was DELETED."
+    }
+    If ($DeleteCheck) {throw}
+}
+Catch {
+    Write-Warning "Unable to DELETE the Project `"$($ProjectName)`" via `"$($OctopusURL)/api/$($SpaceId)/Projects/$($Project.Id)`""
+    Write-Warning "Check your parameters (Octopus API key, URL, SpaceId, etc.), ensure your API key has sufficient permissions, and the Octopus Server is accessible from this machine."
+    break
+}
