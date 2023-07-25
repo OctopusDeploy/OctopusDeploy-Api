619eb03bad822cf1242b10371cca87942ecbe1c2
Ben Pearce <ben.pearce@octopus.com>
2023-07-25 11:03:18 +1000

    added remove role from target example (#165)

---
+++ REST/PowerShell/Targets/RemoveRoleFromTarget.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = "Stop";
+
+# Define working variables
+$octopusURL = "https://youroctourl"
+$octopusAPIKey = "API-YOURAPIKEY"
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+$spaceName = "default"
+$machineName = "MyMachine"
+$targetRole = "MyRole"
+
+# Get space
+$space = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/spaces/all" -Headers $header) | Where-Object {$_.Name -eq $spaceName}
+
+# Get machine
+$machine = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/machines/all" -Headers $header) | Where-Object {$_.Name -eq $machineName}
+
+# Remove target role
+$machine.Roles = $machine.Roles | Where-Object { $_ -ne $targetRole }
+
+Invoke-RestMethod -Method Put -Uri "$octopusURL/api/$($space.Id)/machines/$($machine.Id)" -Body ($machine | ConvertTo-Json -Depth 10) -Headers $header
