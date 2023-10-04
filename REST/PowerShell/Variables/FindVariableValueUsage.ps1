946dea2e44c2077c41f4b326b78986ad7f6f3703
bffoust <48598070+bffoust@users.noreply.github.com>
2023-10-04 09:41:44 -0600

    Update FindVariableValueUsage.ps1
    Add the ability to search all Spaces in Octopus.
    Modified the Variable set API call to include all variable sets, it was only returning the first 33 sets.

--- REST/PowerShell/Variables/FindVariableValueUsage.ps1
+++ REST/PowerShell/Variables/FindVariableValueUsage.ps1
@@ -3,89 +3,93 @@ $ErrorActionPreference = "Stop";
 # Define working variables
 $octopusURL = "https://your.octopus.app"
 $octopusAPIKey = "API-YOURAPIKEY"
+# Get the script directory
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
 $header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
 
-# Specify the Space to search in
-$spaceName = "Default"
+# Specify the Spaces to search
+$spaceNames = @("Default", "YOUROTHERSPACES")
 
-# Specify the Variable Value to find, without OctoStache syntax 
+# Specify the Variable Value to find, without OctoStache syntax
 
 $variableValueToFind = "mytestvalue"
 
-# Optional: set a path to export to csv
-$csvExportPath = ""
-
-$variableTracking = @()
-$octopusURL = $octopusURL.TrimEnd('/')
-
-# Get space
-$space = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/spaces/all" -Headers $header) | Where-Object {$_.Name -eq $spaceName}
-
-Write-Host "Looking for usages of variable value named '$variableValueToFind' in space: '$spaceName'"
-
-# Get variables from variable sets
-$variableSets = Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/libraryvariablesets?contentType=Variables" -Headers $header
-
-foreach ($variableSet in $variableSets.Items)
+foreach ($spaceName in $spaceNames)
 {
-    Write-Host "Checking variable set '$($variableSet.Name)'"
-    
-    $variableSetVariables = Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/variables/variableset-$($variableSet.Id)" -Headers $header
+    # Optional: set a path to export to csv, one file per space
+    $csvExportPath = "$scriptPath/$spaceName-$variableValueToFind.csv"
 
-    $matchingNamedVariables = $variableSetVariables.Variables | Where-Object {$_.Value -like "*$variableValueToFind*"}
-    if($null -ne $matchingNamedVariables){
-        foreach($match in $matchingNamedVariables){
-            $result = [PSCustomObject]@{
-                Project = $null
-                VariableSet = $variableSet.Name
-                MatchType = "Value in Library Set"
-                Context = $match.Value
-                Property = $null
-                AddtionalContext = $match.Name
+    $variableTracking = @()
+    $octopusURL = $octopusURL.TrimEnd('/')
+
+    # Get space
+    $space = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/spaces/all" -Headers $header) | Where-Object {$_.Name -eq $spaceName}
+
+    Write-Host "Looking for usages of variable value named '$variableValueToFind' in space: '$spaceName'"
+
+    # Get variables from variable sets
+    $variableSets = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/libraryvariablesets/all" -Headers $header)
+
+    foreach ($variableSet in $variableSets)
+    {
+        Write-Host "Checking variable set '$($variableSet.Name)'"
+
+        $variableSetVariables = Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/variables/variableset-$($variableSet.Id)" -Headers $header
+
+        $matchingNamedVariables = $variableSetVariables.Variables | Where-Object {$_.Value -like "*$variableValueToFind*"}
+        if($null -ne $matchingNamedVariables){
+            foreach($match in $matchingNamedVariables){
+                $result = [PSCustomObject]@{
+                    Project = $null
+                    VariableSet = $variableSet.Name
+                    MatchType = "Value in Library Set"
+                    Context = $match.Value
+                    Property = $null
+                    AddtionalContext = $match.Name
+                }
+                $variableTracking += $result
             }
-            $variableTracking += $result
+        }
+
+    }
+
+    # Get all projects
+    $projects = Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/projects/all" -Headers $header
+
+    # Loop through projects
+    foreach ($project in $projects)
+    {
+        Write-Host "Checking project '$($project.Name)'"
+        # Get project variables
+        $projectVariableSet = Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/variables/$($project.VariableSetId)" -Headers $header
+
+        # Check to see if variable is named in project variables.
+        $ProjectMatchingNamedVariables = $projectVariableSet.Variables | Where-Object {$_.Value -like "*$variableValueToFind*"}
+        if($null -ne $ProjectMatchingNamedVariables) {
+            foreach($match in $ProjectMatchingNamedVariables) {
+                $result = [pscustomobject]@{
+                    Project = $project.Name
+                    VariableSet = $null
+                    MatchType = "Named Project Variable"
+                    Context = $match.Value
+                    Property = $null
+                    AdditionalContext = $match.Name
+                }
+                # Add to tracking list
+                $variableTracking += $result
+            }
         }
     }
 
-}
 
-# Get all projects
-$projects = Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/projects/all" -Headers $header
-
-# Loop through projects
-foreach ($project in $projects)
-{
-    Write-Host "Checking project '$($project.Name)'"
-    # Get project variables
-    $projectVariableSet = Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/variables/$($project.VariableSetId)" -Headers $header
-
-    # Check to see if variable is named in project variables.
-    $ProjectMatchingNamedVariables = $projectVariableSet.Variables | Where-Object {$_.Value -like "*$variableValueToFind*"}
-    if($null -ne $ProjectMatchingNamedVariables) {
-        foreach($match in $ProjectMatchingNamedVariables) {
-            $result = [pscustomobject]@{
-                Project = $project.Name
-                VariableSet = $null
-                MatchType = "Named Project Variable"
-                Context = $match.Value
-                Property = $null
-                AdditionalContext = $match.Name
-            }
-            
-            # Add to tracking list
-            $variableTracking += $result
+    if($variableTracking.Count -gt 0) {
+        Write-Host ""
+        Write-Host "Found $($variableTracking.Count) results:"
+        $variableTracking
+        if (![string]::IsNullOrWhiteSpace($csvExportPath)) {
+            Write-Host "Exporting results to CSV file: $csvExportPath"
+            $variableTracking | Export-Csv -Path $csvExportPath -NoTypeInformation
         }
-    }
-}
-    
-
-if($variableTracking.Count -gt 0) {
-    Write-Host ""
-    Write-Host "Found $($variableTracking.Count) results:"
-    $variableTracking
-    if (![string]::IsNullOrWhiteSpace($csvExportPath)) {
-        Write-Host "Exporting results to CSV file: $csvExportPath"
-        $variableTracking | Export-Csv -Path $csvExportPath -NoTypeInformation
     }
 }
