e6ef25aa9b4bf94883a0ac966509880e7fa2a181
JChatmasOctopus <50506513+JChatmasOctopus@users.noreply.github.com>
2024-02-21 15:14:35 -0600

    Create GetLatestDeploymentForAllTenants.ps1
    Script to get the latest deployment for all tenants in a space.

---
+++ REST/PowerShell/Tenants/GetLatestDeploymentForAllTenants.ps1
@@ -0,0 +1,48 @@
+$octopusURL = "YOUR OCTOPUS URL"
+$apiKey = "YOUR OCTOPUS API KEY"
+$spaceName = "YOUR SPACE NAME"
+$outputFilePath = "DIRECTORY TO OUTPUT\OctopusTenantsLatestDeployment.csv"
+$headers = @{ "X-Octopus-ApiKey" = $apiKey }
+
+# Get space ID
+$space = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/spaces/all" -Headers $headers) | Where-Object { $_.Name -eq $spaceName }
+$spaceId = $space.Id
+
+# Get all tenants in the specified space
+$tenantListUrl = "$octopusURL/api/$spaceId/tenants/all"
+$tenants = Invoke-RestMethod -Uri $tenantListUrl -Method Get -Headers $headers
+
+# Initialize an array to hold the tenant details
+$results = @()
+
+# Loop through each tenant to find the latest deployment
+foreach ($tenant in $tenants) 
+{
+    $deploymentsUrl = "$octopusURL/api/$spaceId/deployments?tenants=$($tenant.Id)&take=1"
+    $latestDeployment = Invoke-RestMethod -Uri $deploymentsUrl -Method Get -Headers $headers -ErrorAction Stop | Select-Object -ExpandProperty Items | Select-Object -First 1
+
+    if ($latestDeployment) 
+    {
+        # Convert date
+        $deploymentDate = Get-Date $latestDeployment.Created -Format "MMM-d-yyyy"
+        $row = New-Object PSObject -Property @{
+            TenantName = $tenant.Name
+            TenantID = $tenant.Id
+            LastDeploymentDate = $deploymentDate
+        }
+    } 
+    else 
+    {
+        $row = New-Object PSObject -Property @{
+            TenantName = $tenant.Name
+            TenantID = $tenant.Id
+            LastDeploymentDate = "No deployments"
+        }
+    }
+    $results += $row
+}
+
+# Export results to CSV
+$results | Export-Csv -Path $outputFilePath -NoTypeInformation
+
+Write-Host "Export completed. File saved at: $outputFilePath"
