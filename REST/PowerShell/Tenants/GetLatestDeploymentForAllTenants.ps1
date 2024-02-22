755179498572baab3c5418699c4039258c8d0be5
JChatmasOctopus <50506513+JChatmasOctopus@users.noreply.github.com>
2024-02-21 18:38:52 -0600

    Update GetLatestDeploymentForAllTenants.ps1
    Null check on $latestDeployment (thanks @twerthi!)

--- REST/PowerShell/Tenants/GetLatestDeploymentForAllTenants.ps1
+++ REST/PowerShell/Tenants/GetLatestDeploymentForAllTenants.ps1
@@ -15,13 +15,17 @@ $tenants = Invoke-RestMethod -Uri $tenantListUrl -Method Get -Headers $headers
 # Initialize an array to hold the tenant details
 $results = @()
 
+Write-Host "---"
+Write-Host "Calculating the latest deployment for $($tenants.count) tenants (this may take some time!)."
+Write-Host "---"
+
 # Loop through each tenant to find the latest deployment
 foreach ($tenant in $tenants) 
 {
     $deploymentsUrl = "$octopusURL/api/$spaceId/deployments?tenants=$($tenant.Id)&take=1"
     $latestDeployment = Invoke-RestMethod -Uri $deploymentsUrl -Method Get -Headers $headers -ErrorAction Stop | Select-Object -ExpandProperty Items | Select-Object -First 1
 
-    if ($latestDeployment) 
+    if ($null -ne $latestDeployment) 
     {
         # Convert date
         $deploymentDate = Get-Date $latestDeployment.Created -Format "MMM-d-yyyy"
