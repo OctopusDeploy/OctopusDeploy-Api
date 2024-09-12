5ce344003c75f0d792bf9224d47bfeb617ac1850
Mark Harrison <mark.harrison@octopus.com>
2024-09-12 10:38:37 +0100

    Rework main loop to only show users once per project

--- REST/PowerShell/Projects/GetProjectMembers.ps1
+++ REST/PowerShell/Projects/GetProjectMembers.ps1
@@ -7,14 +7,16 @@ $header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
 
 # Provide the space name for the projects to check
 $spaceName = "Default"
-# Provide an optional list of project names to export.
+# Provide an optional list of project names to check.
 $projectNames = @()
+# Show users that are not scoped to any project or project group
+$showUsersIfNoProjectScopes = $False
 
 $octopusURL = $octopusURL.TrimEnd('/')
 
 # Get Space
 $spaces = Invoke-RestMethod -Uri "$octopusURL/api/spaces?partialName=$([uri]::EscapeDataString($spaceName))&skip=0&take=100" -Headers $header 
-$space = $spaces.Items | Where-Object { $_.Name -eq $spaceName }
+$space = $spaces.Items | Where-Object { $_.Name -ieq $spaceName }
 
 # Cache all project groups
 Write-Output "Retrieving all project groups"
@@ -41,7 +43,7 @@ Write-Output "Retrieving all teams"
 $teams = @()
 $response = $null
 do {
-  $uri = if ($response) { $octopusURL + $response.Links.'Page.Next' } else { "$octopusURL/api/$($space.Id)/teams?spaces=$($space.Id)&includeSystem=true" }
+  $uri = if ($response) { $octopusURL + $response.Links.'Page.Next' } else { "$octopusURL/api/teams?spaces=$($space.Id)&includeSystem=true" }
   $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $header
   $teams += $response.Items
 } while ($response.Links.'Page.Next')
@@ -62,41 +64,63 @@ if ($null -ne $projectNames -and $projectNames.Length -gt 0) {
   $projects = $projects | Where-Object { $projectNames -icontains $_.Name }
 }
 
-$teamScopedUserRoles = @{}
+$teamsUserRoles = @{}
 function Get-ScopedUserRolesForTeam($teamId, $spaceId) {
-  $response = $null
-  if ($null -ne $teamScopedUserRoles[$teamId]) {
-    $response = $teamScopedUserRoles[$teamId] 
+  $scopedUserRolesForTeam = $teamsUserRoles[$teamId]
+  if ($null -eq $scopedUserRolesForTeam) {
+    $response = Invoke-RestMethod -Uri "$octopusURL/api/teams/$teamId/scopeduserroles" -Headers $header 
+    $scopedUserRolesForTeam = $response.Items
+    $teamsUserRoles[$teamId] = $scopedUserRolesForTeam
   }
-  else {
-    $scopedUserRolesForTeam = Invoke-RestMethod -Uri "$octopusURL/api/teams/$teamId/scopeduserroles" -Headers $header 
-    $teamScopedUserRoles[$teamId] = $scopedUserRolesForTeam.Items
-  }
-  $itemsForSpace = $response | Where-Object { $_.SpaceId -eq $spaceId }
-  return $itemsForSpace
+  
+  return $scopedUserRolesForTeam
 }
+$foundNoProjectMembers = $false
 
 # Main loop
 foreach ($project_group in $project_groups) {
-  Write-Output "Project Group: $($project_group.Name)"
+  Write-Output "Project Group: '$($project_group.Name)'"
 
   $projects_in_group = $projects | Where-Object { $_.ProjectGroupId -eq $project_group.Id }
   foreach ($project in $projects_in_group) {
     Write-Output "  Project: $($project.Name)"
-
+    $projectMembers = @{}
     foreach ($team in $teams) {
       $scoped_user_roles = Get-ScopedUserRolesForTeam -teamId $team.Id -spaceId $space.Id
-      $has_project_scoping = $scoped_user_roles | Where-Object { $_.ProjectIds -contains $project.Id }
-      $has_project_group_scoping = $scoped_user_roles | Where-Object { $_.ProjectGroupIds -contains $project_group.Id }
-      if ($has_project_scoping -or $has_project_group_scoping) {
-        Write-Output "    Team: $($team.Name)"
-        $team.MemberUserIds | ForEach-Object { 
-          $userId = $_
-          $user = $users | Where-Object { $_.Id -eq $userId } | Select-Object -First 1 
-          Write-Host "      User: $($user.DisplayName)" -ForegroundColor Green
+      # Check for explicit project scopes
+      $has_project_scoping = @($scoped_user_roles | Where-Object { $_.ProjectIds -contains $project.Id }).Length -gt 0
+      $has_project_group_scoping = @($scoped_user_roles | Where-Object { $_.ProjectGroupIds -contains $project_group.Id }).Length -gt 0
+      $output_no_project_scopes = ($showUsersIfNoProjectScopes -and -not $has_project_scoping -and -not $has_project_group_scoping)
+
+      if ($null -ne $scoped_user_roles) {
+        if ($has_project_scoping -or $has_project_group_scoping -or $output_no_project_scopes) {
+          $team.MemberUserIds | ForEach-Object { 
+            $userId = $_
+            if ($projectMembers[$_] -eq $null) {
+              $user = $users | Where-Object { $_.Id -eq $userId } | Select-Object -First 1 
+              $projectMembers[$_] = @{
+                DisplayName = $user.DisplayName;
+                Teams       = @($team.Name);
+              }
+            }
+            else {
+              $projectMembers[$_].Teams += $team.Name;
+            }
+          }
         }
       }
-      
+    }
+    if ($projectMembers.Count -eq 0) {
+      $foundNoProjectMembers = $true
+    }
+    else {
+      foreach ($userId in $projectMembers.Keys) {
+        $projectMember = $projectMembers[$userId]
+        Write-Host "    User: $($projectMember.DisplayName) (in Teams: $($projectMember.Teams -Join ", "))" -ForegroundColor Green
+      }
     }
   }
+}
+if ($foundNoProjectMembers) {
+  Write-Host "One or more project(s) had no explicit user roles scoped to them. Set `$showUsersIfNoProjectScopes`=True to display all users." -ForegroundColor Yellow
 }
