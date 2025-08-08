276f3b2a53b525cb51b5c43f0a71e40be0488595
patrick-smergut-octopus <83607909+patrick-smergut-octopus@users.noreply.github.com>
2025-08-08 13:41:28 -0700

    Update RestoreTemplateFromEarlierVersion.ps1
    Minor code cleanup

--- REST/PowerShell/StepTemplates/RestoreTemplateFromEarlierVersion.ps1
+++ REST/PowerShell/StepTemplates/RestoreTemplateFromEarlierVersion.ps1
@@ -11,7 +11,7 @@ $versionToRestore = "#" # Set to the old version of the template you want restor
 $headers = @{ "X-Octopus-ApiKey" = $ApiKey }
 
 # Get the desired version of the template
-$stepTemplate = (Invoke-RestMethod -Method Get -Uri "$OctopusServerUrl/api/$SpaceId/actiontemplates/$myTemplateId/versions/$versionToRestore" -Headers $headers)
+$stepTemplate = Invoke-RestMethod -Method Get -Uri "$OctopusServerUrl/api/$SpaceId/actiontemplates/$myTemplateId/versions/$versionToRestore" -Headers $headers
 
 # Put the old template back to restore it
 Invoke-RestMethod -Method Put -Uri "$OctopusServerUrl/api/$SpaceId/actiontemplates/$myTemplateId" -Body ($stepTemplate | ConvertTo-Json -Depth 10) -Headers $headers
