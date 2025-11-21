3f4575d4314658df533b04de5cd373fcda192244
Britton Riggs <104870596+briggs-octo@users.noreply.github.com>
2025-11-21 08:46:18 -0700

    Implementing functions and better error handling

--- REST/PowerShell/TaskLogs/TaskLogsSearch.ps1
+++ REST/PowerShell/TaskLogs/TaskLogsSearch.ps1
@@ -22,64 +22,86 @@
 $octopusUrl = "https://your-octopus-url"
 $apiKey = "API-x"
 $spaceName = "Default" 
-$searchKeyword = "search keyword" # The text to find
-$taskLimit = 20 # Max number of recent tasks to scan. 
+$searchKeyword = "search keywords" # The text to find
+$taskLimit = 20 # Max number of recent tasks to scan
 
 $header = @{ "X-Octopus-ApiKey" = $apiKey }
 
-# Get the list of recent deployment tasks
-Write-Host "Fetching last $taskLimit tasks..." -ForegroundColor Cyan
+function Get-SpaceId {
+    $spacesEndpoint = "$octopusUrl/api/spaces/all"
 
-$space = (Invoke-RestMethod -Method Get -Uri "$octopusUrl/api/spaces/all" -Headers $header) | Where-Object { $_.Name -eq $spaceName }
-$spaceId = $space.Id
-
-$tasksEndpoint = "$octopusUrl/api/$spaceId/tasks?skip=0&take=$taskLimit&name=Deploy"
-
-try {
-    $response = Invoke-RestMethod -Uri $tasksEndpoint -Headers $header -Method Get
-    $tasks = $response.Items
-}
-catch {
-    Write-Error $_.Exception
-    exit
-}
-
-if ($tasks.Length -eq 0) {
-    Write-Host "No tasks found."
-    exit
-}
-
-# Loop through tasks, download raw log, and search
-Write-Host "Searching logs for keyword: '$searchKeyword'" -ForegroundColor Yellow
-$matchesFound = 0
-
-foreach ($task in $tasks) {
-    $taskId = $task.Id
-    $description = $task.Description
-    
-    # Visual progress indicator
-    Write-Host -NoNewline "." 
-
-    # Fetch the RAW log (returns plain text)
-    $rawLogEndpoint = "$octopusUrl/api/$spaceId/tasks/$taskId/raw"
-    
     try {
-        $logContent = Invoke-RestMethod -Uri $rawLogEndpoint -Headers $header -Method Get
-        
-        if ($logContent -and ($logContent -match [regex]::Escape($searchKeyword))) {
-            Write-Host "`n[MATCH FOUND] $description" -ForegroundColor Green
-            
-            # Construct the UI Link
-            $taskWebLink = "$octopusUrl/app#/$spaceId/tasks/$taskId"
-            Write-Host "Link: $taskWebLink" 
-            Write-Host "---------------------------------------------------"
-            $matchesFound++
-        }
+        $space = (Invoke-RestMethod -Method Get -Uri $spacesEndpoint -Headers $header) | Where-Object { $_.Name -eq $spaceName }
+        $spaceId = $space.Id
+    } 
+    catch {
+        Write-Host "Error getting Space Id"
+        Write-Error $_.Exception
+        exit
+    }
+
+    return $spaceId
+}
+
+function Get-Tasks($spaceId) {
+    Write-Host "Fetching last $taskLimit tasks..." -ForegroundColor Cyan
+    
+    $tasksEndpoint = "$octopusUrl/api/$spaceId/tasks?skip=0&take=$taskLimit&name=Deploy"
+
+    try {
+        $response = Invoke-RestMethod -Uri $tasksEndpoint -Headers $header -Method Get
+        $tasks = $response.Items
     }
     catch {
-        Write-Host $rawLogEndpoint
-        Write-Host $_.Exception
+        Write-Error $_.Exception
+        exit
     }
+
+    if ($tasks.Length -eq 0) {
+        Write-Host "No tasks found."
+        exit
+    }
+
+    return $tasks
 }
 
-Write-Host "`nSearch complete. Found $matchesFound matches." -ForegroundColor Cyan
+function Search-TaskLogs($spaceId) {
+    # Loop through tasks, download raw log, and search
+    Write-Host "Searching logs for keyword: '$searchKeyword'" -ForegroundColor Yellow
+    $matchesFound = 0
+
+    foreach ($task in $tasks) {
+        $taskId = $task.Id
+        $description = $task.Description
+        
+        # Visual progress indicator
+        Write-Host -NoNewline "." 
+
+        # Fetch the RAW log (returns plain text)
+        $rawLogEndpoint = "$octopusUrl/api/$spaceId/tasks/$taskId/raw"
+        
+        try {
+            $logContent = Invoke-RestMethod -Uri $rawLogEndpoint -Headers $header -Method Get
+            
+            if ($logContent -and ($logContent -match [regex]::Escape($searchKeyword))) {
+                Write-Host "`n[MATCH FOUND] $description" -ForegroundColor Green
+                
+                # Construct the UI Link
+                $taskWebLink = "$octopusUrl/app#/$spaceId/tasks/$taskId"
+                Write-Host "Link: $taskWebLink" 
+                Write-Host "---------------------------------------------------"
+                $matchesFound++
+            }
+        }
+        catch {
+            Write-Host $rawLogEndpoint
+            Write-Error $_.Exception
+        }
+    }
+
+    Write-Host "`nSearch complete. Found $matchesFound matches." -ForegroundColor Cyan
+}
+
+$spaceId = Get-SpaceId
+$tasks = Get-Tasks($spaceId)
+Search-TaskLogs($spaceId)
